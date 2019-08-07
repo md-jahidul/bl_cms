abab5f5f77926682c30754fbdfa11b85ab7c0c10
Nahidul Hasan <nahidul.hasan@bs-23.net>
2019-08-07 16:52:56 +0600

    Updated models and migrations

--- database/migrations/2019_08_07_105146_create_answer_options_table.php
+++ database/migrations/2019_08_07_105146_create_answer_options_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOptionsTable extends Migration
+class CreateAnswerOptionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateOptionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('options', function (Blueprint $table) {
+        Schema::create('answer_options', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateOptionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('options');
+        Schema::dropIfExists('answer_options');
     }
 }
