b3cc191b66066159c1b193267b22f7ab96660106
Nahidul Hasan <nahidul.hasan@bs-23.net>
2019-08-08 09:19:36 +0600

    Added controller and routes for api

---
+++ app/Http/Controllers/API/TestAPIController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Http\Controllers\API;
+
+
+use App\Http\Controllers\Controller;
+
+class TestAPIController extends Controller
+{
+
+    /**
+     * @return string
+     */
+    public function index()
+    {
+        return 'api';
+
+    }
+
+}
