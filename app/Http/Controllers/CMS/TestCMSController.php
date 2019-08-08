1d2f272ad48dd696c49d67b26e0fe964072f2144
Nahidul Hasan <nahidul.hasan@bs-23.net>
2019-08-08 09:25:36 +0600

    Added TestControler and route for CMS

---
+++ app/Http/Controllers/CMS/TestCMSController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\CMS;
+
+use App\Http\Controllers\Controller;
+
+
+class TestCMSController extends Controller
+{
+
+    public function index()
+    {
+        echo 'test';
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/cms', 'CMS\TestCMSController@index');
