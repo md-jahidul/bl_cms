67c2b960c1b46af7e8abab5bbb50dc6f2daa2b3e
Md. Abu Taleb <abutaleb@bs-23.net>
2019-08-22 15:51:15 +0600

    move User model to app folder and layout issue fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,38 @@
-@extends('layouts.app')
+@extends('layouts.master-layout')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+@section('main-content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-md-10">
+                                Profile
+                            </div>
+                            <div class="col-md-2">
+                                <a class="btn-sm btn-primary pull-right" href="" role="button">Edit info</a>
+                            </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
                         </div>
-                    @endif
+                    </div>
 
-                    You are logged in!
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <h5><b>ID : </b> {{auth()->user()->id}}</h5>
+                        <h5><b>Name : </b> {{auth()->user()->name}}</h5>
+                        <h5><b>Role : </b> {{auth()->user()->role}}</h5>
+                        <h5><b>Email : </b> {{auth()->user()->email}}</h5>
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
+
