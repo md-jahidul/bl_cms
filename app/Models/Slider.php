3ddcca24280f80280c6aa85a35679e39e92d1bf3
emdad.ullah <emdad.ullah@bs-23.com>
2019-08-25 11:53:57 +0600

    Slider basic code integration (Pattern yet not integrated)

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Slider extends Model
 {
     protected $fillable = ['slider_type_id', 'title', 'description', 'short_code'];
+
+    public function sliderType(){
+        return $this->belongsTo(SliderType::class);
+    }
+ 
+    public function sliderImages(){
+        return $this->hasMany(SliderImage::class);
+    }
+    
 }
