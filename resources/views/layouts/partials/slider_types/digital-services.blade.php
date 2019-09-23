272f5d359d470f0d7183e36965fcbab3e26244ae
emdad.ullah <emdad.ullah@bs-23.com>
2019-09-23 13:23:45 +0600

    wrong name issue fix & add bangla for hero & digital service slider

--- resources/views/layouts/partials/slider_types/digital-services.blade.php
+++ resources/views/layouts/partials/slider_types/digital-services.blade.php
@@ -1,6 +1,16 @@
 <h4 class="pl-1">Advance Option</h4>
 <div class="form-actions col-md-12 mt-0"></div>
 
+<div class="form-group col-md-4 {{ $errors->has('title_bn') ? ' error' : '' }}">
+    <label for="title_bn" class="">Title Bangla</label>
+    <input type="text" name="title_bn"  class="form-control" placeholder="Enter price info"
+            value="{{ (!empty($other_attributes['title_bn'])) ? $other_attributes['title_bn'] : "" }}" >
+    <div class="help-block"></div>
+    @if ($errors->has('title_bn'))
+        <div class="help-block">  {{ $errors->first('title_bn') }}</div>
+    @endif
+</div>
+
 <div class="form-group col-md-4 {{ $errors->has('price_info') ? ' error' : '' }}">
     <label for="price_info" class="">Price Info</label>
     <input type="text" name="price_info"  class="form-control" placeholder="Enter price info"
@@ -10,6 +20,17 @@
         <div class="help-block">  {{ $errors->first('price_info') }}</div>
     @endif
 </div>
+
+<div class="form-group col-md-4 {{ $errors->has('price_info_bn') ? ' error' : '' }}">
+    <label for="price_info_bn" class="">Price Info</label>
+    <input type="text" name="price_info_bn"  class="form-control" placeholder="Enter price info (Bangla)"
+            value="{{ (!empty($other_attributes['price_info_bn'])) ? $other_attributes['price_info_bn'] : "" }}" >
+    <div class="help-block"></div>
+    @if ($errors->has('price_info_bn'))
+        <div class="help-block">  {{ $errors->first('price_info_bn') }}</div>
+    @endif
+</div>
+
 <div class="form-group col-md-4 {{ $errors->has('google_play_link') ? ' error' : '' }}">
     <label for="title" class="">Google Play Store Link</label>
     <input type="text" name="google_play_link"  class="form-control" placeholder="Google Play Store Link"
@@ -29,13 +50,13 @@
     @endif
 </div>
 
-<div class="form-group col-md-12 {{ $errors->has('details_page_link') ? ' error' : '' }}">
-    <label for="details_page_link" class="">Details Page Link</label>
-    <input type="url" name="details_page_link"  class="form-control" placeholder="Enter details page link"
-           value="{{ (!empty($other_attributes['details_page_link'])) ? $other_attributes['details_page_link'] : "" }}">
+<div class="form-group col-md-12 {{ $errors->has('description_bn') ? ' error' : '' }}">
+    <label for="description_bn" class="required">Description</label>
+    <textarea type="text" name="description_bn" rows="5"  class="form-control" placeholder="Enter alt text"
+                required data-validation-required-message="Please select start date">{{ old("title") ? old("title") : '' }}</textarea>
     <div class="help-block"></div>
-    @if ($errors->has('details_page_link'))
-        <div class="help-block">  {{ $errors->first('details_page_link') }}</div>
+    @if ($errors->has('description_bn'))
+        <div class="help-block">  {{ $errors->first('description_bn') }}</div>
     @endif
 </div>
 
