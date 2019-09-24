605c3bd97e7fdba8f26844a833233030c1ad0973
BS289 <nahidul.hasan@bs-23.net>
2019-09-24 10:08:17 +0600

    Added Slider Image Service

---
+++ app/Services/SliderImageService.php
@@ -0,0 +1,84 @@
+<?php
+namespace App\Services;
+
+
+use App\Repositories\SliderImageRepository;
+use App\Repositories\SliderRepository;
+use App\Traits\CrudTrait;
+use Illuminate\Http\Response;
+
+class SliderImageService
+{
+    use CrudTrait;
+
+    /**
+     * @var $sliderRepository
+     */
+    protected $sliderImageRepository;
+
+    /**
+     * SliderImageService constructor.
+     * @param SliderImageRepository $sliderImageRepository
+     */
+    public function __construct(SliderImageRepository $sliderImageRepository)
+    {
+        $this->sliderImageRepository = $sliderImageRepository;
+        $this->setActionRepository($sliderImageRepository);
+    }
+
+
+    public function itemList($sliderId, $type)
+    {
+        return $this->sliderImageRepository->getSliderImage($sliderId, $type);
+    }
+
+    /**
+     * @param $data
+     * @return Response
+     */
+    public function storeSliderImage($data, $sliderId)
+    {
+        $count = count($this->sliderImageRepository->findAll());
+        $imageUrl = $this->imageUpload($data, 'image_url', $data['title'], 'slider-images');
+        $data['image_url'] = env('APP_URL', 'http://localhost:8000') . "/slider-images/".$imageUrl;
+        $data['slider_id'] = $sliderId;
+        $data['sequence'] = ++$count;
+        $this->save($data);
+        return new Response('Slider Image added successfully');
+    }
+
+    public function tableSortable($data)
+    {
+        $this->sliderImageRepository->sliderImageTableSort($data);
+        return new Response('update successfully');
+    }
+
+    /**
+     * @param $data
+     * @param $id
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|Response
+     */
+    public function updateSliderImage($data, $id)
+    {
+        $sliderImage = $this->findOne($id);
+        if (!empty($data['image_url'])){
+            $imageUrl = $this->imageUpload($data, 'image_url', $data['title'], 'slider-images');
+            $data['image_url'] = env('APP_URL', 'http://localhost:8000') . "/slider-images/".$imageUrl;
+        }
+        $sliderImage->update($data);
+        return Response('Slider Image update successfully !');
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|Response
+     * @throws \Exception
+     */
+    public function deleteSliderImage($id)
+    {
+        $sliderImage = $this->findOne($id);
+        $sliderImage->delete();
+        return Response('Slider Image delete successfully');
+    }
+
+}
