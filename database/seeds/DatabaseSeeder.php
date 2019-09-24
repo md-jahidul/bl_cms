2e2eb9e33135aa5d5e9d9c114deebd135f362c19
BS289 <nahidul.hasan@bs-23.net>
2019-09-24 16:20:24 +0600

    Added seeder file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -25,5 +25,15 @@ class DatabaseSeeder extends Seeder
         $this->call(SliderImageTableSeeder::class);
         $this->call(DigitalServiceTableSeeder::class);
         $this->call(SettingKeysTableSeeder::class);
+
+        $this->call(InternetOfferSeeder::class);
+        $this->call(MixedBundleOfferSeeder::class);
+        $this->call(NearbyOfferSeeder::class);
+        $this->call(ShortcutsTableSeeder::class);
+        $this->call(WelcomeInfoSeeder::class);
+        $this->call(BannersTableSeeder::class);
+        $this->call(CurrentBalanceSeeder::class);
+        $this->call(BonusTableSeeder::class);
+        $this->call(ContextualCardSeeder::class);
     }
 }
