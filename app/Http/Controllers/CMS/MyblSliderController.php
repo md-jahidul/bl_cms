7ed3e933d1d88e9d5ef9aac925a4eb79362a1a38
Md. Tafhim Faisal Chowdhury <tafhim.chowdhury@bs-23.net>
2019-09-25 13:22:09 +0600

    bug fixed

--- app/Http/Controllers/CMS/MyblSliderController.php
+++ app/Http/Controllers/CMS/MyblSliderController.php
@@ -108,9 +108,9 @@ class MyblSliderController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(MyblSliderRequest $request,Slider $slider)
+    public function update(MyblSliderRequest $request,$id)
     {
-        session()->flash('success',$this->sliderService->updateSlider($request, $slider)->getContent());
+        session()->flash('success',$this->sliderService->updateSlider($request, $this->sliderService->findOne($id))->getContent());
         return redirect(route('myblslider.index'));
     }
 
