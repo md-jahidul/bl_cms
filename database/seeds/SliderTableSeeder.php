dc8d3f22ace540a645e9f0c83ad7765c59e03cd9
emdad.ullah <emdad.ullah@bs-23.com>
2019-09-25 16:25:24 +0600

    Code refactor

--- database/seeds/SliderTableSeeder.php
+++ database/seeds/SliderTableSeeder.php
@@ -17,19 +17,22 @@ class SliderTableSeeder extends Seeder
         $sliders = ['Hero', 'Digital Services', 'Testimonial'];
 
         foreach ($sliders as $key => $slider){
-            Slider::create([
+
+            $other_attributes = [
+                'sliding_speed' => 10,
+                'view_list_btn_text_en' => "View all $slider",
+                'view_list_btn_text_bn' => "সমস্ত পরিষেবা দেখুন",
+                'view_list_url' => "/view-all-digital-service",
+            ];
+
+            $slider = Slider::create([
                 'title' =>  'Home page ' . $slider,
                 'component_id' => ++$key,
                 'description' => 'Description of ' . $slider,
                 'short_code' => '[slider_'.++$key .']',
                 'platform' => 'web',
-                'other_attributes' => ($slider == 'Digital Services') ? [
-                    'sliding_speed' => 10,
-                    'view_list_btn_text_en' => "View all $slider",
-                    'view_list_btn_text_bn' => "সমস্ত পরিষেবা দেখুন",
-                    'view_list_url' => "/view-all-digital-service",
-                ] : []
-            ]);
+                'other_attributes' => ($slider == 'Digital Services') ? $other_attributes : []
+            ]);            
         }
     }
 }
