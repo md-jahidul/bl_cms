b785be4acd65276831b56d287b52c3b65a2c62ef
Charlie <thwoi.hla@brainstation23.com>
2019-09-26 18:26:23 +0600

    custom error pages added

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.error-layout')
+@section('error-content')
+    <div id="notfound">
+        <div class="notfound">
+            <div class="notfound-404">
+                <h1>Oops!</h1>
+                <h2>419 - Sorry, your session has expired. Please refresh and try again.</h2>
+            </div>
+            <a href="/home">Go TO Homepage</a>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.error-layout')
+@section('error-content')
+    <div id="notfound">
+        <div class="notfound">
+            <div class="notfound-404">
+                <h1>Oops!</h1>
+                <h2>500 - Something Went Wrong.Contact with Support Team</h2>
+            </div>
+            <a href="/home">Go TO Homepage</a>
+        </div>
+    </div>
+@endsection
