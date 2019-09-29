a4122e3df0da4752edc36752e485702fbcc8e97b
BS289 <nahidul.hasan@bs-23.net>
2019-09-29 11:42:26 +0600

    Added migrations

--- app/Http/Controllers/CMS/PushNotificationController.php
+++ app/Http/Controllers/CMS/PushNotificationController.php
@@ -17,28 +17,21 @@ class PushNotificationController extends Controller
      */
     public function sendNotification(Request $request)
     {
-       dd($request->input('title'));
+        $notification = [
+            'title' => $request->input('title'),
+            'body' => $request->input('message'),
+            'send_to_type​' => "​ALL",
+            "is_interactive" => "Yes",
+            "data" => [
+                "cid" => "1",
+                "url" => "test.com",
+                "component" => "offer",
+            ]
 
-       $data = [
-           'title' =>  $request->input('title'),
-           'body'  =>   $request->input('title'),
 
        ];
 
-
-
-       $data ['title'] = $request->input('title');
-
-        $data ['body'] = $request->input('title');
-
-        "title"​ : ​ " সরা অফার!" ,
-"body"​ : " ​ বাংলািলংক আপনার জন িনেয় এেসেছ ১ পয়সা/ সেক
-"send_to_type"​ : ​ "ALL"​ ,
-"is_interactive"​ : ​ "'Yes"​ ,
-"data"​ : {
-"cid"​ : ​ "1"​ ,
-"url"​ : ​ "https://xxxxx.xxx"​ ,
-"component"​ : ​ "offer"
+        dd($notification);
 
     }
 
