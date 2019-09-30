6930b47ab8cd19b520ab273bfe973dd6edeb25ed
BS289 <nahidul.hasan@bs-23.net>
2019-09-30 14:23:21 +0600

    Updated migrations

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,7 +18,8 @@ class UsersTableSeeder extends Seeder
             'email' => 'all-admin@admin.com',
             'phone' => '0191911111541',
             'uid' => uniqid(),
-            'password' =>Hash::make('123456')
+            'password' =>Hash::make('123456'),
+            'device_token' => '122'
         ]);
         DB::table('users')->insert([
             'name' => 'mybl-admin',
@@ -26,7 +27,8 @@ class UsersTableSeeder extends Seeder
             'email' => 'mybl-admin@admin.com',
             'phone' => '0191911454111',
             'uid' => uniqid(),
-            'password' => Hash::make('123456')
+            'password' => Hash::make('123456'),
+            'device_token' => '122'
         ]);
         DB::table('users')->insert([
             'name' => 'web-admin',
@@ -34,7 +36,18 @@ class UsersTableSeeder extends Seeder
             'email' => 'web-admin@admin.com',
             'phone' => '0191941551111',
             'uid' => uniqid(),
-            'password' => Hash::make('123456')
+            'password' => Hash::make('123456'),
+            'device_token' => '122'
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Rafiqul Hasan',
+            'role_id' => 5,
+            'email' => 'web-admin@admin.com',
+            'phone' => '01919415568',
+            'uid' => uniqid(),
+            'password' => Hash::make('123456'),
+            'device_token' =>  "cbccwirrVwU:APA91bEJZkLi9mWV5hh6EjtFFLegw6_f4_eBGlqJ02KnHo7cW4KuyfJZIfQ-_VEDdCr3Kf3Lg9kj9e7ihELO3aHGrlZJxYGsOTPObHjEOLSAJPAOm_KI9QpvQM28wPW0D3BK2MllIMv2"
         ]);
     }
 }
