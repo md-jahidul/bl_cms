4f7fba18a7e45828dcfdb385203ef5400a4fbb3e
BS289 <nahidul.hasan@bs-23.net>
2019-10-23 14:51:39 +0600

    MBA-302: add service and repository

---
+++ app/Repositories/AppVersionRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\AppVersion;
+
+/**
+ * Class AppVersionRepository
+ * @package App\Repositories
+ */
+class AppVersionRepository
+{
+    /**
+     * @var Banner
+     */
+    protected $model;
+
+    /**
+     * AppVersionRepository constructor.
+     * @param AppVersion $model
+     */
+    public function __construct(AppVersion $model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Retrieve Banner info
+     *
+     * @return mixed
+     */
+    public function getAppVersion()
+    {
+        return $this->model->get();
+    }
+
+}
---
+++ app/Services/AppVersionService.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Services;
+
+use App\Repositories\AppVersionRepository;
+
+class AppVersionService
+{
+
+    /**
+     * @var AppVersionRepository
+     */
+    protected $appVersionRepository;
+
+
+    /**
+     * AppVersionService constructor.
+     * @param AppVersionRepository $appVersionRepository
+     */
+    public function __construct(AppVersionRepository $appVersionRepository)
+    {
+         $this->appVersionRepository = $appVersionRepository;
+    }
+
+
+
+}
