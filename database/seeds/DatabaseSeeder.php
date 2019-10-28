904270ac6698ca7c7387bd4cac45bf8f7c026445
Charlie <thwoi.hla@brainstation23.com>
2019-10-28 13:03:39 +0600

    privacy-policy seeder add

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -67,5 +67,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(MyBlSliderSeeder::class);
         $this->call(ShortcutSeeder::class);
+        $this->call(PrivacyPolicySeeder::class);
     }
 }
