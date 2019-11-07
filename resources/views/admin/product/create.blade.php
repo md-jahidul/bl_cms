544cf2ae12b22ea7513ea9530171c16c98eb27f0
jahidul <jahidul@bs-23.net>
2019-11-07 11:53:53 +0600

    product create page bug fixed

--- resources/views/admin/product/create.blade.php
+++ resources/views/admin/product/create.blade.php
@@ -117,7 +117,7 @@
                                 <div class="form-group col-md-6">
                                     <label for="tag_category_id">Tag</label>
                                     <select class="form-control" name="tag_category_id">
-                                        <option value="0">---Select Tag---</option>
+                                        <option value="">---Select Tag---</option>
                                         @foreach($tags as $tag)
                                             <option value="{{ $tag->id }}">{{ $tag->name_en }}</option>
                                         @endforeach
