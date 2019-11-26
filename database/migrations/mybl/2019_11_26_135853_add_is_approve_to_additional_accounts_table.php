0a0f8f3cdba89d64b78ba4d7fab6ee369b575095
BS289 <nahidul.hasan@bs-23.net>
2019-11-26 20:04:40 +0600

    add new column in additionalaccount table

---
+++ database/migrations/mybl/2019_11_26_135853_add_is_approve_to_additional_accounts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsApproveToAdditionalAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('additional_accounts', function (Blueprint $table) {
+            $table->tinyInteger('is_approve');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('additional_accounts', function (Blueprint $table) {
+            $table->dropColumn('paid');
+        });
+    }
+}
