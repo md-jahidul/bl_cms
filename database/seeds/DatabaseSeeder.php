7fd3202ae45d45c916d23986203640759868d524
Charlie <thwoi.hla@brainstation23.com>
2019-12-09 16:14:34 +0600

    my product category reomve

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -74,6 +74,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(MyBlInternetOfferCategorySeeder::class);
         $this->call(TelescopepanelUserSeeder::class);
-        $this->call(MyBlProductCategorySeeder::class);
+/*        $this->call(MyBlProductCategorySeeder::class);*/
     }
 }
