63c9530f846f2f0db44c9bcc7795c79e31fe998a
Md. Jahangir Alam <jahangir.alam@bs-23.com>
2019-12-22 20:01:31 +0600

    add migration for full text search

---
+++ database/migrations/assetlite/2019_12_22_060616_add_fulltext_index_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFulltextIndexProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            DB::statement('ALTER TABLE products ADD FULLTEXT search(name_en, name_bn)');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
