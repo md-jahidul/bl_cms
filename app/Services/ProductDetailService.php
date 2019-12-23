4d9fc49233875b13503b6bafd02f931c7a668fc5
rchowdhury-bs23 <r.chowdhury@brainstation-23.com>
2019-12-23 19:10:53 +0600

    temp changes

--- app/Services/ProductDetailService.php
+++ app/Services/ProductDetailService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\OtherRelatedProduct;
 use App\Models\RelatedProduct;
+use App\Models\ProductDetail;
 use App\Repositories\PartnerOfferDetailRepository;
 use App\Repositories\ProductDetailRepository;
 use App\Traits\CrudTrait;
@@ -89,13 +90,16 @@ class ProductDetailService
      */
     public function updateProductDetails($data, $id)
     {
-        $productDetails = $this->findOne($id);
+        $productDetails = ProductDetail::find($id);
+
         if (!empty($data['banner_image_url'])) {
             $data['banner_image_url'] = $this->upload($data['banner_image_url'], 'assetlite/images/banner/product_details');
         }
-        if (!empty($data['other_attributes'])) {
-            $data['other_attributes'] = str_replace(" ", "_", strtolower($data['other_attributes']));
+        
+        if( !isset( $data['other_attributes'] ) ){
+            $data['other_attributes'] = null;
         }
+
         $productDetails->update($data);
 
         return Response('Product Details update successfully!');
