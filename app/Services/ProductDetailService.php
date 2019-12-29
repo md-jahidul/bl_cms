b27367737c8345d1549a4bb1484e3012965ce815
jahidul <jahidul@bs-23.net>
2019-12-29 20:37:49 +0600

    bondho sim offer details issue fixed

--- app/Services/ProductDetailService.php
+++ app/Services/ProductDetailService.php
@@ -6,8 +6,11 @@ use App\Models\OtherRelatedProduct;
 use App\Models\RelatedProduct;
 use App\Repositories\PartnerOfferDetailRepository;
 use App\Repositories\ProductDetailRepository;
+use App\Repositories\ProductRepository;
 use App\Traits\CrudTrait;
 use App\Traits\FileTrait;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Response;
 
 
 class ProductDetailService
@@ -20,13 +23,19 @@ class ProductDetailService
      */
     protected $productDetailRepository;
 
+    protected $productRepository;
+
     /**
-     * PartnerOfferDetailService constructor.
-     * @param PartnerOfferDetailRepository $partnerOfferDetailRepository
+     * ProductDetailService constructor.
+     * @param ProductDetailRepository $productDetailRepository
+     * @param ProductRepository $productRepository
      */
-    public function __construct(ProductDetailRepository $productDetailRepository)
-    {
+    public function __construct(
+        ProductDetailRepository $productDetailRepository,
+        ProductRepository $productRepository
+    ) {
         $this->productDetailRepository = $productDetailRepository;
+        $this->productRepository = $productRepository;
         $this->setActionRepository($productDetailRepository);
     }
 
@@ -82,20 +91,32 @@ class ProductDetailService
     }
 
     /**
-     * Update product details page
-     * @param  [type] $data [Request all]
-     * @param  [type] $id   [description]
-     * @return RedirectResponse
+     * @param $data
+     * @param $productId
+     * @return ResponseFactory|Response
      */
     public function updateProductDetails($data, $productId)
     {
         $productDetails = $this->productDetailRepository->findOneByProperties(['product_id' => $productId]);
+
         if (!empty($data['banner_image_url'])) {
             $data['banner_image_url'] = $this->upload($data['banner_image_url'], 'assetlite/images/banner/product_details');
         }
         if (!empty($data['other_attributes'])) {
             $data['other_attributes'] = str_replace(" ", "_", strtolower($data['other_attributes']));
         }
+
+
+        $bondhoSimOffers = $this->productRepository->countBondhoSimOffer();
+
+
+        foreach ($bondhoSimOffers as $bondhoSimOffer) {
+            $productDetails = $this->productDetailRepository->findOneByProperties(['product_id' => $bondhoSimOffer->id]);
+
+//            dd($productDetails);
+            $productDetails->update($data);
+        }
+
         $productDetails->update($data);
 
         return Response('Product Details update successfully!');
