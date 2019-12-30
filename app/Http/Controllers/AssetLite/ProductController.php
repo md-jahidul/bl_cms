8277682372bf381345014f143a58f33f7744b653
jahidul <jahidul@bs-23.net>
2019-12-30 17:07:51 +0600

    startup offer bug fix

--- app/Http/Controllers/AssetLite/ProductController.php
+++ app/Http/Controllers/AssetLite/ProductController.php
@@ -239,6 +239,9 @@ class ProductController extends Controller
     {
         $products = $this->productService->findRelatedProduct($type, $id);
         $productDetail = $this->productService->detailsProduct($id);
+
+//        return $productDetail;
+
         return view('admin.product.product_details', compact('type', 'productDetail', 'products'));
     }
 
@@ -250,6 +253,8 @@ class ProductController extends Controller
      */
     public function productDetailsUpdate(Request $request, $type, $id)
     {
+
+
         //$productDetails = $this->productDetailService->findOne($request->product_details_id);
 
         $this->productDetailService->updateOtherRelatedProduct($request, $id);
