66d3075cf53fb700a864abce16a88299410fff6f
Charlie <thwoi.hla@brainstation23.com>
2020-01-01 19:38:36 +0600

    product serveic

--- app/Models/ProductCore.php
+++ app/Models/ProductCore.php
@@ -43,7 +43,8 @@ class ProductCore extends Model
         'validity_in_days',
         'purchase_option',
         'is_rate_cutter_offer',
-        'assetlite_offer_type'
+        'assetlite_offer_type',
+        'app_offer_section'
 
     ];
 
