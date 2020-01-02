b11a4cd53494d3b9eda6b8714ca8a7fdf173d77a
BS289 <nahidul.hasan@bs-23.net>
2020-01-02 13:02:32 +0600

    Update mybl slider type

--- database/seeds/MyblSliderComponentTypesTableSeeder.php
+++ database/seeds/MyblSliderComponentTypesTableSeeder.php
@@ -12,7 +12,7 @@ class MyblSliderComponentTypesTableSeeder extends Seeder
      */
     public function run()
     {
-        $slider_types = ['Home', 'Dashboard'];
+        $slider_types = ['Home', 'Dashboard', 'Internet Package', 'Mixed Bundle', 'Usage History'];
 
         $slider_component_types = [];
 
