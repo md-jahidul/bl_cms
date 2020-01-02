471acd1d4026ae7666d4e40083e06d9cc716fd50
BS289 <nahidul.hasan@bs-23.net>
2020-01-02 13:06:55 +0600

    Minor change

--- database/seeds/MyblSliderComponentTypesTableSeeder.php
+++ database/seeds/MyblSliderComponentTypesTableSeeder.php
@@ -12,7 +12,7 @@ class MyblSliderComponentTypesTableSeeder extends Seeder
      */
     public function run()
     {
-        $slider_types = ['Home', 'Dashboard', 'Internet Package', 'Mixed Bundle', 'Usage History'];
+        $slider_types = ['Home', 'Dashboard', 'Internet', 'Bundle', 'History'];
 
         $slider_component_types = [];
 
