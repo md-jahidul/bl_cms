076c71c41569a1b902a912b523447d656e16989e
jahidul <jahidul@bs-23.net>
2020-01-07 16:53:31 +0600

    prepaid plans details add editor

--- resources/views/layouts/partials/product-details/packages/prepaid_plan.blade.php
+++ resources/views/layouts/partials/product-details/packages/prepaid_plan.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group col-md-6 {{ $errors->has('details_en') ? ' error' : '' }}">
     <label for="details_en" class="required">Offer Details (English)</label>
     <textarea type="text" name="details_en"  class="form-control" placeholder="Enter details in English" rows="4"
-              required data-validation-required-message="Enter offer details in English">{{ $productDetail->product_details->details_en }}</textarea>
+              required data-validation-required-message="Enter offer details in English" id="details">{{ $productDetail->product_details->details_en }}</textarea>
     <div class="help-block"></div>
     @if ($errors->has('details_en'))
         <div class="help-block">{{ $errors->first('details_en') }}</div>
@@ -11,7 +11,7 @@
 <div class="form-group col-md-6 {{ $errors->has('details_bn') ? ' error' : '' }}">
     <label for="details_bn" class="required">Offer Details (Bangla)</label>
     <textarea type="text" name="details_bn"  class="form-control" placeholder="Enter offer details in English" rows="4"
-              required data-validation-required-message="Enter offer details in English">{{ $productDetail->product_details->details_bn }}</textarea>
+              required data-validation-required-message="Enter offer details in English" id="details">{{ $productDetail->product_details->details_bn }}</textarea>
     <div class="help-block"></div>
     @if ($errors->has('details_bn'))
         <div class="help-block">{{ $errors->first('details_bn') }}</div>
