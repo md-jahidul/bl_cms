7e38e11e13901688653e3fdd5ce82e281c978b4a
Raihan Chowdhury <r.chowdhury@brainstation-23.com>
2020-02-16 16:54:21 +0600

    ecarrer form submit

---
+++ database/migrations/assetlite/2020_02_16_043620_create_university_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUniversityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('universities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('university_name');
+            $table->string('university_slug')->nullable();
+            $table->tinyInteger('is_active')->default(1)->comment('active = 1, inactive = 0');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('universities');
+    }
+}
