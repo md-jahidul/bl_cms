c210b1b28ca955359fec904bb7fd0d38d4746a95
jahidul <jahidul@bs-23.net>
2020-03-10 17:02:30 +0600

    new component type add

--- public/js/custom-js/component.js
+++ public/js/custom-js/component.js
@@ -1,17 +1,25 @@
 (function () {
     var inputText = $('#input-text');
+    var extraTitle = $('#extra-title');
+
+
     var textArea = $('#text-area');
     var textEditor = $('#text-editor');
     var dropDown = $('#dropdown');
     var imageField = $('#image-field');
     var multiImage = $('#multi-image');
+    var buttonCheck = $('#button-check');
 
     var textField = $('#text-field');
+    var extraTitleField = $('#extra-title-field');
+
     var textAreaField = $('#text-area-field');
     var textEditorField = $('#text-editor-field');
     var dropdownField = $('#dropdown_field');
     var singleImage = $('#single-image');
     var multipleImageField = $('#multiple-image-field');
+
+    var textAreaButton = $('#text-area-button');
 
     // $('#data-type').change(function () {
     //     alert();
@@ -29,10 +37,13 @@
     }
 
     showHideElement(inputText, textField);
+    showHideElement(extraTitle, extraTitleField);
+
     showHideElement(textArea, textAreaField);
     showHideElement(textEditor, textEditorField);
     showHideElement(dropDown, dropdownField);
     showHideElement(imageField, singleImage);
     showHideElement(multiImage, multipleImageField);
+    showHideElement(buttonCheck, textAreaButton);
 })();
 
