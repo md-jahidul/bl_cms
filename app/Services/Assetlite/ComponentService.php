d34f46d2a2e8b6de1fa8cebdc6a3c9f6d47b8c58
jahangiralambs <jahangir.alam@bs-23.com>
2020-03-15 07:23:07 +0000

    component service conflict resolve

--- app/Services/Assetlite/ComponentService.php
+++ app/Services/Assetlite/ComponentService.php
@@ -14,104 +14,6 @@ use App\Repositories\ComponentRepository;
 
 class ComponentService
 {
-<<<<<<< HEAD
-	use CrudTrait;
-	use FileTrait;
-
-	const APP = 1;
-	const VAS = 2;
-	const PAGE_TYPE = 'app_services';
-
-	/**
-	 * @var $componentRepository
-	 */
-	protected $componentRepository;
-
-	/**
-	 * AppServiceProductService constructor.
-	 * @param ComponentRepository $componentRepository
-	 */
-	public function __construct(ComponentRepository $componentRepository)
-	{
-		$this->componentRepository = $componentRepository;
-		$this->setActionRepository($componentRepository);
-	}
-
-	public function findByType($type)
-	{
-		return $this->componentRepository->findOneByProperties(['type' => $type]);
-	}
-
-	public function componentList($section_id)
-	{
-		return $this->componentRepository->findByProperties(['section_details_id' => $section_id]);
-	}
-
-	/**
-	 * @param $data
-	 * @return Response
-	 */
-	public function storeComponentDetails($data)
-	{
-		if (request()->hasFile('image_url')) {
-			$data['image'] = $this->upload($data['image_url'], 'assetlite/images/app-service/product/details');
-		}
-
-		if( request()->filled('other_attr') ){
-			$other_attributes = request()->input('other_attr', null);
-			$data['other_attributes'] = !empty($other_attributes) ? json_encode($other_attributes) : null;
-		}
-
-		if (request()->hasFile('video_url')) {
-			$data['video'] = $this->upload($data['video_url'], 'assetlite/video/app-service/product/details');
-			$data['other_attributes'] = json_encode(['video_type' => 'uploaded_video']);
-		} elseif( request()->filled('video_url') ) {
-			$data['video'] = request()->input('video_url', null);
-			$data['other_attributes'] = json_encode(['video_type' => 'youtube_video']);
-		}
-	
-	   $data['page_type'] = self::PAGE_TYPE;
-		
-		$results = [];
-		if (isset($data['multi_item']) && !empty($data['multi_item'])) {
-			$request_multi = $data['multi_item'];
-			$item_count = isset($data['multi_item_count']) ? $data['multi_item_count'] : 0;
-			for ($i = 1; $i <= $item_count; $i++) {
-				foreach ($data['multi_item'] as $key => $value) {
-					$sub_data = [];
-					$check_index = explode('-', $key);
-					if ($check_index[1] == $i) {
-						if (request()->hasFile('multi_item.' . $key)) {
-							$value = $this->upload($value, 'assetlite/images/app-service/product/details');
-						}
-						$results[$i][$check_index[0]] = $value;
-					}
-				}
-			}
-		}
-
-		$data['multiple_attributes'] = json_encode($results);
-
-		$this->save($data);
-		return new Response('App Service Component added successfully');
-	}
-
-	// protected function imageUpload($data)
-	// {
-	// 	if (isset($data['multiple_attributes']) && !empty($data['multiple_attributes'])) {
-	// 		$countImage = count($data['multiple_attributes']['image']);
-	// 		for ($i = 1; $i <= $countImage; $i++) {
-	// 			foreach ($data['multiple_attributes']['image'] as $key => $value) {
-	// 				if (!empty($value)) {
-	// 					$imageUrl = $this->upload($value, 'assetlite/images/product_details');
-	// 					$image[$key] = $imageUrl;
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// 	return $image;
-	// }
-=======
     use CrudTrait;
     use FileTrait;
 
@@ -201,8 +103,6 @@ class ComponentService
 //        return $image;
 //    }
 
-
->>>>>>> ac20c5c22e5e4cce526f6ca9db2401d177cab456
 
     public function componentStore($data, $sectionId)
     {
@@ -416,8 +316,6 @@ class ComponentService
 	}
 
 
-<<<<<<< HEAD
-=======
     /**
      * @param $data
      * @return Response
@@ -440,5 +338,5 @@ class ComponentService
         $appServiceCat->delete();
         return Response('Component deleted successfully !');
     }
->>>>>>> ac20c5c22e5e4cce526f6ca9db2401d177cab456
+
 }
