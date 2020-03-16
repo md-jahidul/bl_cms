1d3461653ccde9193551a36464b0ad0154dfdaaa
jahidul <jahidul@bs-23.net>
2020-03-16 14:40:44 +0600

    issue fix

--- resources/views/admin/product/details/components/create.blade.php
+++ resources/views/admin/product/details/components/create.blade.php
@@ -125,7 +125,7 @@
                                                 <slot id="extra-title-field" class="d-none">
                                                     <div class="form-group col-md-6 {{ $errors->has('extra_title_en') ? ' error' : '' }}">
                                                         <label for="extra_title_en">Extra Title (English)</label>
-                                                        <textarea type="text" name="extra_title_en"  class="form-control" placeholder="Enter extra title in English" id="details"></textarea>
+                                                        <textarea type="text" name="extra_title_en"  class="form-control" placeholder="Enter extra title in English"></textarea>
                                                         <div class="help-block"></div>
                                                         @if ($errors->has('extra_title_en'))
                                                             <div class="help-block">{{ $errors->first('extra_title_en') }}</div>
@@ -134,7 +134,7 @@
 
                                                     <div class="form-group col-md-6 {{ $errors->has('extra_title_bn') ? ' error' : '' }}">
                                                         <label for="extra_title_bn">Extra Title (Bangla)</label>
-                                                        <textarea type="text" name="extra_title_bn"  class="form-control" placeholder="Enter extra title in Bangla"  id="details"></textarea>
+                                                        <textarea type="text" name="extra_title_bn"  class="form-control" placeholder="Enter extra title in Bangla"></textarea>
                                                         <div class="help-block"></div>
                                                         @if ($errors->has('extra_title_bn'))
                                                             <div class="help-block">{{ $errors->first('extra_title_bn') }}</div>
