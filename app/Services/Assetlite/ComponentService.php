1ee568d790ea2ec5ab8eb1cf7c2051b735cd0ba1
jahidul <jahidul@bs-23.net>
2020-03-16 12:59:01 +0600

    issue fix app service component

--- app/Services/Assetlite/ComponentService.php
+++ app/Services/Assetlite/ComponentService.php
@@ -72,7 +72,7 @@ class ComponentService
             $data['other_attributes'] = json_encode(['video_type' => 'youtube_video']);
         }
 
-        $data['page_type'] = self::PAGE_TYPE;
+        $data['page_type'] = self::PAGE_TYPE['app_services'];
 
         $results = [];
         if (isset($data['multi_item']) && !empty($data['multi_item'])) {
@@ -159,33 +159,64 @@ class ComponentService
     public function componentUpdate($data, $id)
     {
 
+
         $component = $this->findOne($id);
 
+//        $results = [];
+//        if (isset($data['multi_item']) && !empty($data['multi_item'])) {
+//            $request_multi = $data['multi_item'];
+//            $item_count = isset($data['multi_item_count']) ? $data['multi_item_count'] : 0;
+//            for ($i = 1; $i <= $item_count; $i++) {
+//                foreach ($data['multi_item'] as $key => $value) {
+//                    $sub_data = [];
+//                    $check_index = explode('-', $key);
+//                    if ($check_index[1] == $i) {
+//                        if (request()->hasFile('multi_item.' . $key)) {
+//                            $value = $this->upload($value, 'assetlite/images/product_details');
+//                        }
+//                        $results[$i][$check_index[0]] = $value;
+//                    }
+//                }
+//            }
+//        }
+
+
+//        dd($results);
+
         // get original data
-        $new_multiple_attributes = $component->multiple_attributes;
+//        $new_multiple_attributes = $component->multiple_attributes;
 
         // contains all the inputs from the form as an array
-        $input_multiple_attributes = isset($data['multiple_attributes']) ? $data['multiple_attributes'] : null;
+//        $input_multiple_attributes = $results;
+//        $input_multiple_attributes = isset($data['multi_item']) ? $data['multi_item'] : null;
+
+//        dd($input_multiple_attributes);
 
         // loop over the product array
-        if ($input_multiple_attributes) {
-            foreach ($input_multiple_attributes as $data_id => $inputData) {
-                foreach ($inputData as $key => $value) {
-                    // set the new value
-                    $new_multiple_attributes[$data_id][$key] = is_object($value) ? $this->upload($value, 'assetlite/images/product_details') : $value;
-                }
-            }
-        }
+//        if ($input_multiple_attributes) {
+//            foreach ($input_multiple_attributes as $data_id => $inputData) {
+//
+//
+//                foreach ($inputData as $key => $value) {
+//
+//                    // set the new value
+//                    $new_multiple_attributes[$data_id][$key] = $value;
+////                    $new_multiple_attributes[$data_id][$key] = $value;
+//
+////                    $new_multiple_attributes[$data_id][$key] = is_object($value) ? $this->upload($value, 'assetlite/images/product_details') : $value;
+//                }
+//
+//                dd($new_multiple_attributes);
+//            }
+//        }
 
-        if (request()->hasFile('image')) {
-            $data['image'] = $this->upload($data['image'], 'assetlite/images/banner/product_details');
-        }
 
 
-        $data['multiple_attributes'] = $new_multiple_attributes;
+//        dd($new_multiple_attributes);
 
 
-        $data['page_type'] = "other_offer";
+//        $data['multiple_attributes'] = $new_multiple_attributes;
+
         $component->update($data);
         return response("Component update successfully!!");
     }
