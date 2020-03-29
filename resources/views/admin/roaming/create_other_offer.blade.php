410069e4611f24fd07412f893b4c9c55cc1ef173
unknown <mahmudbd1000@gmail.com>
2020-03-29 20:52:01 +0600

    bug fix of roaming module 2

--- resources/views/admin/roaming/create_other_offer.blade.php
+++ resources/views/admin/roaming/create_other_offer.blade.php
@@ -155,12 +155,14 @@
 
 @push('style')
 <link rel="stylesheet" href="{{asset('plugins')}}/sweetalert2/sweetalert2.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Dropify/0.2.2/css/dropify.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/css/bootstrap-multiselect.css">
 <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/editors/summernote.css') }}">
 
 @endpush
 @push('page-js')
 <script src="{{asset('plugins')}}/sweetalert2/sweetalert2.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Dropify/0.2.2/js/dropify.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-multiselect/0.9.15/js/bootstrap-multiselect.min.js"></script>
 <script src="{{ asset('app-assets/vendors/js/editors/summernote/summernote.js') }}" type="text/javascript"></script>
 
@@ -181,6 +183,18 @@ $(function () {
         ],
         height: 170
     });
+    
+    
+//show dropify for  photo
+    $('.dropify').dropify({
+        messages: {
+            'default': 'Browse for photo',
+            'replace': 'Click to replace',
+            'remove': 'Remove',
+            'error': 'Choose correct file format'
+        }
+    });
+
 
 
 
