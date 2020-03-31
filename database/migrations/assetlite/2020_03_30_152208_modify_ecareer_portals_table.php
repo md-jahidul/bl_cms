3b9dba48b7a9ef96a6e2507c8ad8f7d91b73fab3
unknown <mahmudbd1000@gmail.com>
2020-03-31 11:59:10 +0600

    change migration class name

--- database/migrations/assetlite/2020_03_30_152208_modify_ecareer_portals_table.php
+++ database/migrations/assetlite/2020_03_30_152208_modify_ecareer_portals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ModifyEcareerPortalsTable extends Migration {
+class ChangeEcareerPortalsTable extends Migration {
 
     /**
      * Run the migrations.
