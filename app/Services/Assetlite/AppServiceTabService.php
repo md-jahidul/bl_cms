db90de4aef17de07a24af701f2f33bf712d0456c
jahidul <jahidul@bs-23.net>
2020-04-07 13:33:12 +0600

    app and service tab SEO required field add

--- app/Services/Assetlite/AppServiceTabService.php
+++ app/Services/Assetlite/AppServiceTabService.php
@@ -44,9 +44,43 @@ class AppServiceTabService
     public function updateTabs($data, $id)
     {
         $appServiceTabs = $this->findOne($id);
-        if (request()->hasFile('banner_image_url')) {
-            $data['banner_image_url'] = $this->upload($data['banner_image_url'], 'assetlite/images/banner/app-service-tab');
+
+        if (!empty($data['banner_image_url'])) {
+            //delete old web photo
+            if ($data['old_web_img'] != "") {
+                $this->deleteFile($data['old_web_img']);
+            }
+            $photoName = $data['banner_name'] . '-web';
+            $data['banner_image_url'] = $this->upload($data['banner_image_url'], 'assetlite/images/banner/app-service-tab', $photoName);
+            $status = $data['banner_image_url'];
         }
+
+        if (!empty($data['banner_image_mobile'])) {
+            //delete old web photo
+            if ($data['old_mob_img'] != "") {
+                $this->deleteFile($data['old_mob_img']);
+            }
+            $photoName = $data['banner_name'] . '-mobile';
+            $data['banner_image_mobile'] = $this->upload($data['banner_image_mobile'], 'assetlite/images/banner/app-service-tab', $photoName);
+            $status = $data['banner_image_mobile'];
+        }
+
+        //only rename
+        if ($data['old_banner_name'] != $data['banner_name']) {
+            if (empty($data['banner_image_url']) && $data['old_web_img'] != "") {
+                $fileName = $data['banner_name'] . '-web';
+                $directoryPath = 'assetlite/images/banner/app-service-tab';
+                $data['banner_image_url'] = $this->rename($data['old_web_img'], $fileName, $directoryPath);
+                $status = $data['banner_image_url'];
+            }
+            if (empty($data['banner_image_mobile']) && $data['old_mob_img'] != "") {
+                $fileName = $data['banner_name'] . '-mobile';
+                $directoryPath = 'assetlite/images/banner/app-service-tab';
+                $data['banner_image_mobile'] = $this->rename($data['old_mob_img'], $fileName, $directoryPath);
+                $status = $data['banner_image_mobile'];
+            }
+        }
+
         $appServiceTabs->update($data);
         return Response('App Service Tab updated successfully');
     }
