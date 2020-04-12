962479fbbde186398cebdf94567cea6cffeec437
jahidul <jahidul@bs-23.net>
2020-04-12 12:46:55 +0600

    add SEO field in product

--- resources/views/layouts/partials/products/common-field/search-related-field.blade.php
+++ resources/views/layouts/partials/products/common-field/search-related-field.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group col-md-12 {{ $errors->has('banner_image_url') ? ' error' : '' }}">
     <label> URL (url slug) <span class="text-danger">*</span></label>
-    <input type="text" class="form-control" value="{{ isset($productDetails->url_slug) ? $productDetails->url_slug : null }}" required name="url_slug" placeholder="URL">
+    <input type="text" class="form-control" value="{{ isset($product->url_slug) ? $product->url_slug : null }}" required name="url_slug" placeholder="URL">
     <small class="text-info">
         <strong>i.e:</strong> 1000Min-15GB-1000SMS (no spaces)<br>
     </small>
@@ -8,7 +8,7 @@
 
 <div class="form-group col-md-6 {{ $errors->has('alt_text') ? ' error' : '' }}">
     <label>Page Header (HTML)</label>
-    <textarea class="form-control" rows="7" name="page_header">{{ isset($productDetails->page_header) ? $productDetails->page_header : null }}</textarea>
+    <textarea class="form-control" rows="7" name="page_header">{{ isset($product->page_header) ? $product->page_header : null }}</textarea>
     <small class="text-info">
         <strong>Note: </strong> Title, meta, canonical and other tags
     </small>
@@ -16,7 +16,7 @@
 
 <div class="form-group col-md-6 {{ $errors->has('alt_text') ? ' error' : '' }}">
     <label>Schema Markup</label>
-    <textarea class="form-control" rows="7" name="schema_markup">{{ isset($productDetails->schema_markup) ? $productDetails->schema_markup : null }}</textarea>
+    <textarea class="form-control" rows="7" name="schema_markup">{{ isset($product->schema_markup) ? $product->schema_markup : null }}</textarea>
     <small class="text-info">
         <strong>Note: </strong> JSON-LD (Recommended by Google)
     </small>
