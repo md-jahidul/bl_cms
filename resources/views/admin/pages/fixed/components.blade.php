6c180a4bf543a7681589447b4e50015c38120200
jahidul <jahidul@bs-23.net>
2020-04-28 16:40:52 +0600

    component issue fix

--- resources/views/admin/pages/fixed/components.blade.php
+++ resources/views/admin/pages/fixed/components.blade.php
@@ -19,7 +19,7 @@
                     <table class="table table-striped table-bordered">
                         <thead>
                         <tr>
-                            <td width="3%">#</td>
+                            <td width="3%"><i class="icon-cursor-move icons"></i></td>
                             <th>Component Type</th>
 {{--                            <th>Component Status</th>--}}
                             <th class="text-right">Action</th>
@@ -28,7 +28,7 @@
                         <tbody id="sortable">
                             @foreach($shortCodes as $index=>$shortCode)
                                 <tr data-index="{{ $shortCode->id }}" data-position="{{ $shortCode->display_order }}">
-                                    <td width="3%">{{ $index + 1 }}</td>
+                                    <td width="3%"><i class="icon-cursor-move icons"></i></td>
                                     <td>{{ $shortCode->component_title }}{!! $shortCode->is_active == 0 ? '<span class="inactive"> ( Inactive )</span>' : '' !!}</td>
 {{--                                    <td>--}}
 {{--                                        @if($shortCode->is_active == 1)--}}
