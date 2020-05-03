bc99df94ed62af4ea32b728a57b17310cfbb6249
Charlie <thwoi.hla@brainstation23.com>
2020-05-03 18:58:59 +0600

    login bonus log

--- database/migrations/mybl/2020_05_03_113601_create_sign_in_bonus_logs_table.php
+++ database/migrations/mybl/2020_05_03_113601_create_sign_in_bonus_logs_table.php
@@ -21,6 +21,7 @@ class CreateSignInBonusLogsTable extends Migration
             $table->string('msisdn');
             $table->date('date');
             $table->string('status')->default('000');
+            $table->json('request_data')->nullable();
             $table->timestamps();
         });
     }
