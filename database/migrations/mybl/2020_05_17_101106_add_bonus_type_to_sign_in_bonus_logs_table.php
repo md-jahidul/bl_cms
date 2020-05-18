34d993def594584d0def06f104ee79bdffb18c20
Charlie <thwoi.hla@brainstation23.com>
2020-05-18 09:54:27 +0600

    migration add. failed jobs.modify sign in bonus logs table

---
+++ database/migrations/mybl/2020_05_17_101106_add_bonus_type_to_sign_in_bonus_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class AddBonusTypeToSignInBonusLogsTable
+ */
+class AddBonusTypeToSignInBonusLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sign_in_bonus_logs', function (Blueprint $table) {
+            $table->string('bonus_type')->after('msisdn')->default('signIn');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sign_in_bonus_logs', function (Blueprint $table) {
+            $table->dropColumn('bonus_type');
+        });
+    }
+}
