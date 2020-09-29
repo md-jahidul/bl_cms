95ac6e6e7e84e1eb48f55b42adf91d7a24b4ade1
jahidul <jahidul@bs-23.net>
2020-09-29 22:14:50 +0600

    AppServiceProvider file add URL forceScheme function

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+//use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +27,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+//       dd(App::environment('production'));
+
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
+
+
         $mainPath = database_path('migrations');
         $directories = glob($mainPath . '/*', GLOB_ONLYDIR);
         $paths = array_merge([$mainPath], $directories);
