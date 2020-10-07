dca59679476c07e6ddb10bb9b9bca0ef3ca73c57
Ahsan Habib <ahabib@bs-23.net>
2020-10-07 16:08:35 +0600

    MBA-3432 : database migration file change

--- database/migrations/2020_10_06_151246_create_support_message_ratings_table.php
+++ database/migrations/2020_10_06_151246_create_support_message_ratings_table.php
@@ -16,7 +16,7 @@ class CreateSupportMessageRatingsTable extends Migration
         Schema::create('support_message_ratings', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('msisdn')->nullable(false);
-            $table->integer('ticketId')->nullable(false);
+            $table->integer('ticket_id')->nullable(false);
             $table->float('ratings',8,2)->nullable(false);
             $table->string('category_name')->nullable();
             $table->string('complaint_location')->nullable();
