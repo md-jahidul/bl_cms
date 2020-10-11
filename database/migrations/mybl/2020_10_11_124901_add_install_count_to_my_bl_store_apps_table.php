f9be13b1ae6a256cffaa614e045f2afea7ee76f5
Nahid <nahidul.hasan@bs-23.net>
2020-10-11 12:54:07 +0600

    add new migration

---
+++ database/migrations/mybl/2020_10_11_124901_add_install_count_to_my_bl_store_apps_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddInstallCountToMyBlStoreAppsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('my_bl_store_apps', function (Blueprint $table) {
+            $table->integer('install_count')->nullable()->after('display_order');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('my_bl_store_apps', function (Blueprint $table) {
+            $table->dropColumn('install_count');
+        });
+    }
+}
