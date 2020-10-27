e7c7b81c02ebdcf800c26cbaf3c31fa98128b214
jahidul <jahidul@bs-23.net>
2020-10-27 20:56:03 +0600

    Referral table modify

--- database/migrations/2020_10_22_163706_create_al_referral_codes_table.php
+++ database/migrations/2020_10_22_163706_create_al_referral_codes_table.php
@@ -15,8 +15,10 @@ class CreateAlReferralCodesTable extends Migration
     {
         Schema::create('al_referral_codes', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('app_id')->nullable();
             $table->string('mobile_no')->nullable();
             $table->string('referral_code')->nullable();
+            $table->integer('share_count')->default(0);
             $table->timestamps();
         });
     }
