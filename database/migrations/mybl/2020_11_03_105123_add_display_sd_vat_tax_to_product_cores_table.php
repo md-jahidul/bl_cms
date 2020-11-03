14d136723ba0b9348163a482275c31a1aa069826
Nahid <nahidul.hasan@bs-23.net>
2020-11-03 10:54:01 +0600

    add migrations

---
+++ database/migrations/mybl/2020_11_03_105123_add_display_sd_vat_tax_to_product_cores_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDisplaySdVatTaxToProductCoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('product_cores', function (Blueprint $table) {
+            $table->integer('display_sd_vat_tax')->nullable()->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('product_cores', function (Blueprint $table) {
+            $table->dropColumn('display_sd_vat_tax');
+        });
+    }
+}
