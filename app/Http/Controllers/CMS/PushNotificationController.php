e68e08b28bc5cab586e9aafe15b23e8518ef74a4
Ahsan Habib <ahabib@bs-23.net>
2020-11-16 15:47:10 +0600

    MBA-3664 : notification body data format change

--- app/Http/Controllers/CMS/PushNotificationController.php
+++ app/Http/Controllers/CMS/PushNotificationController.php
@@ -162,7 +162,7 @@ class PushNotificationController extends Controller
 
                 $customar = $this->customerService->getCustomerList($request, $user_phone, $notification_id);
                 $notification = $this->prepareDataForSendNotification($request, $customar, $notification_id);
-             dd($notification);
+            //  dd($notification);
                 // $notification = $this->getNotificationArray($request, $user_phone);
                 NotificationSend::dispatch($notification, $notification_id, $customar, $this->notificationService)
                     ->onQueue('notification');
