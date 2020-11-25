1c1d0cc0b86f977ce73268d0834e11dd9f39c1cb
Ahsan Habib <ahabib@bs-23.net>
2020-11-25 11:31:38 +0600

    MBA-3892 : deep link create process 30% done

---
+++ app/Http/Controllers/CMS/ProductDeepLinkController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ProductDeepLink;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ProductDeepLinkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ProductDeepLink  $productDeepLink
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductDeepLink $productDeepLink)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ProductDeepLink  $productDeepLink
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductDeepLink $productDeepLink)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ProductDeepLink  $productDeepLink
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductDeepLink $productDeepLink)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ProductDeepLink  $productDeepLink
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductDeepLink $productDeepLink)
+    {
+        //
+    }
+}
