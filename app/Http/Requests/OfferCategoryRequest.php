f1c97d77b6b78936b9c0fc777ae342ab7b529b71
ikhtiaruddin <ikhtiar.uddin@bs-23.net>
2020-12-03 16:49:12 +0600

    AL-2998: Added image name and alt text for both language at offer categories table

---
+++ app/Http/Requests/OfferCategoryRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Controllers\AssetLite\ConfigController;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+
+class OfferCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(Request $request)
+    {
+        $id = $this->route()->parameters()['offer_category'];
+        $image_upload_size = ConfigController::adminImageUploadSize();
+        $image_upload_type = ConfigController::adminImageUploadType();
+
+        return [
+            'banner_name' => !empty($request->banner_name) ? 'regex:/^\S*$/u' : '',
+            'url_slug' => 'required|regex:/^\S*$/u|unique:offer_categories,url_slug,' . $id,
+            'url_slug_bn' => 'required|regex:/^\S*$/u|unique:offer_categories,url_slug_bn,' . $id,
+            'banner_name' => 'required|regex:/^\S*$/u|unique:offer_categories,banner_name,' . $id,
+            'banner_name_web_bn' => 'required|regex:/^\S*$/u|unique:offer_categories,banner_name_web_bn,' . $id,
+            'banner_name_mobile_en' => 'required|regex:/^\S*$/u|unique:offer_categories,banner_name_mobile_en,' . $id,
+            'banner_name_mobile_bn' => 'required|regex:/^\S*$/u|unique:offer_categories,banner_name_mobile_bn,' . $id,
+            'banner_image_url' => 'mimes:' . $image_upload_type . '|max:' . $image_upload_size // 2M
+        ];
+    }
+}
