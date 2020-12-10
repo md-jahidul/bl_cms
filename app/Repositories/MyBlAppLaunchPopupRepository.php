4a56c7730ea46c0f77aa43836aaf25e0e7a1010f
masattar-bs <abdus.sattar@bs-23.net>
2020-12-10 15:32:00 +0600

    MBA-3966: Show schedule information and visibility status in index page

--- app/Repositories/MyBlAppLaunchPopupRepository.php
+++ app/Repositories/MyBlAppLaunchPopupRepository.php
@@ -3,9 +3,11 @@
 namespace App\Repositories;
 
 use App\Models\MyBlAppLaunchPopup;
+use App\RecurringSchedule;
 
 class MyBlAppLaunchPopupRepository extends BaseRepository
 {
 
     protected $modelName = MyBlAppLaunchPopup::class;
+
 }
--- resources/views/admin/app-launch-popup/index.blade.php
+++ resources/views/admin/app-launch-popup/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('action')
     <a href="{{ route('app-launch.new') }}" class="btn btn-info btn-sm btn-glow px-2">
-       Add New
+        Add New
     </a>
 @endsection
 
@@ -20,27 +20,73 @@
                                 <th>Sl</th>
                                 <th>Title</th>
                                 <th>Type</th>
-                                <th>Start Time</th>
-                                <th>End Time</th>
+                                <th>Connection Type</th>
+                                <th>Visibility</th>
+                                <th>Schedule Information</th>
                                 <th>Action</th>
                             </tr>
                             </thead>
                             <tbody>
-                            @foreach($pop_ups as $key=>$popup)
+                            @foreach($pop_ups as $key => $popup)
+                                @php
+                                    $recurringType = $popup->recurring_type;
+                                @endphp
                                 <tr>
                                     <td>{{ ++$key }}</td>
                                     <td>{{ $popup->title }}</td>
                                     <td>{{ strtoupper($popup->type) }}</td>
-                                    <td>{{ \Carbon\Carbon::parse($popup->start_date)->setTimezone('Asia/Dhaka')->toDayDateTimeString() }}</td>
-                                    <td>{{ \Carbon\Carbon::parse($popup->end_date)->setTimezone('Asia/Dhaka')->toDayDateTimeString() }}</td>
+                                    <td>{{ strtoupper($popup->connection_type) }}</td>
+                                    <td>
+                                        @php
+                                            echo $popup->visibilityStatus() ? "<span class='badge badge-success'>Visible</span>"
+                                               : "<span class='badge badge-danger'>Not Visible</span>";
+                                        @endphp
+                                    </td>
+                                    <td>
+                                        Recurring Type: <strong>{{ ucwords($recurringType) }}</strong><br>
+                                        @if($recurringType == 'none')
+                                            Start Time:
+                                            <strong>
+                                                {{ \Carbon\Carbon::parse($popup->start_date)->setTimezone('Asia/Dhaka')->toDayDateTimeString() }}
+                                            </strong><br>
+                                            End Time:
+                                            <strong>
+                                                {{ \Carbon\Carbon::parse($popup->end_date)->setTimezone('Asia/Dhaka')->toDayDateTimeString() }}
+                                            </strong>
+
+                                        @else
+                                            @php
+                                                $schedule = $popup->schedule;
+                                                $timeSlots = $popup->timeSlots;
+                                            @endphp
+                                            @if($recurringType == 'weekly')
+                                                Weekdays: <strong>{{strtoupper($schedule->weekdays)}}</strong><br>
+                                            @endif
+                                            @if($recurringType == 'monthly')
+                                                Month dates:  <strong>{{strtoupper($schedule->month_dates)}}</strong>
+                                                <br>
+                                            @endif
+                                            Time slots:
+                                            @foreach($timeSlots as $timeSlot)
+                                                <span class="badge badge-info">
+                                                    {{\Carbon\Carbon::parse($timeSlot->start_time)->format('h:i: A')}} -
+                                                    {{\Carbon\Carbon::parse($timeSlot->end_time)->format('h:i: A')}}
+                                                </span>
+                                            @endforeach
+
+                                        @endif
+                                    </td>
                                     <td>
                                         <div class="btn-group" role="group">
                                             <form action="{{route('app-launch.delete', $popup->id)}}" method="post">
                                                 {{csrf_field()}}
                                                 {{ method_field('delete') }}
-                                                <button class="btn btn-sm btn-icon btn-outline-danger delete" type="submit"><i class="la la-remove"></i></button>
+                                                <button class="btn btn-sm btn-icon btn-outline-danger delete"
+                                                        type="submit"><i class="la la-remove"></i></button>
                                             </form>
-                                            <a href="{{ route('app-launch.edit', $popup->id) }}" class="btn btn-sm btn-icon btn-outline-success edit" title="edit"><i class="la la-eye"></i></a>
+                                            <a href="{{ route('app-launch.edit', $popup->id) }}"
+                                               class="btn btn-sm btn-icon btn-outline-success edit" title="edit"><i
+                                                    class="la la-eye"></i></a>
                                         </div>
                                     </td>
                                 </tr>
