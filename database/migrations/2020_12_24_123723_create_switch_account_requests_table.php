8f308c39323c0fa1940e1ba32a6fac0c4f8af7c5
masattar-bs <abdus.sattar@bs-23.net>
2020-12-24 14:42:00 +0600

    Switch account reqeust migration added

---
+++ database/migrations/2020_12_24_123723_create_switch_account_requests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSwitchAccountRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('switch_account_requests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('notification_id');
+            $table->string('from_msisdn', 20);
+            $table->string('to_msisdn', 20);
+            $table->tinyInteger('status')->default(2);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('switch_account_requests');
+    }
+}
