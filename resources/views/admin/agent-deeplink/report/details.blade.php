0d31b8e365d3fe981bbf9e080863225f50357569
Ahsan Habib <ahabib@bs-23.net>
2020-12-31 13:24:01 +0600

    add new two option in agent deeplink report export in csv and excel also added in detail page

--- resources/views/admin/agent-deeplink/report/details.blade.php
+++ resources/views/admin/agent-deeplink/report/details.blade.php
@@ -90,6 +90,13 @@
         .dataTable {
             width: 100% !important;
         }
+
+        .dt-buttons.btn-group {
+            margin-bottom: 2px;
+        }
+        div#question_list_table_length {
+            margin-bottom: -50px;
+        }
     </style>
 @endpush
 
@@ -99,10 +106,11 @@
 
     <script>
         $(function () {
-
             $('#question_list_table').DataTable({
                 processing: true,
                 serverSide: true,
+                lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]],
+                pageLength: 10,
                 ajax: "{{ route('agent.deeplink.report.details',$deeplinkId) }}",
                 columns: [
                     {data: 'DT_RowIndex', name: 'DT_RowIndex'},
@@ -116,10 +124,24 @@
                         orderable: true,
                         searchable: true
                     },
+                ],
+                dom: 'Blfrtip',
+                buttons:  [
+                    {
+                        extend: 'csv',
+                        exportOptions: {
+                            columns: [ 1,2,3,4,5 ]
+                        }
+                    },
+                    {
+                        extend: 'excel',
+                        exportOptions: {
+                            columns: [ 1,2,3,4,5 ]
+                        }
+                    }
                 ]
             });
             $(document).on('change', '#filter_category', function (e) {
-                console.log('change');
                 $('#question_list_table').DataTable().ajax.reload();
             });
         });
