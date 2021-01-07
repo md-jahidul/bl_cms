32a98b0e73346820bd3e678ab541e6f0d3ad8ee3
Ahsan Habib <ahabib@bs-23.net>
2021-01-07 18:33:54 +0600

    agent deeplink email sending mechanism has been done also add date time in agent logg details page

--- resources/views/admin/otp-config/create.blade.php
+++ resources/views/admin/otp-config/create.blade.php
@@ -6,7 +6,7 @@
     <li class="breadcrumb-item active">
         @if(isset($config))
             Edit OTP Config
-        @else
+            @else
             Create OTP Config
         @endif
     </li>
@@ -19,7 +19,7 @@
             <h1 class="card-title">
                 @if(isset($config))
                     Edit OTP Config
-                @else
+                    @else
                     Create OTP Config
                 @endif
             </h1>
@@ -29,83 +29,73 @@
         <div class="card-body">
 
             @if(isset($config))
-                <form novalidate action="{{ route('otp-config.update',$config->id) }}" method="post"
-                      enctype="multipart/form-data">
-                    @else
-                        <form novalidate action="{{ route('otp-config.store') }}" method="post"
-                              enctype="multipart/form-data">
-                            @endif
+                <form novalidate action="{{ route('otp-config.update',$config->id) }}" method="post" enctype="multipart/form-data">
+                @else
+                <form novalidate action="{{ route('otp-config.store') }}" method="post" enctype="multipart/form-data">
+            @endif
 
-                            @csrf
-                            @if(isset($config))
-                                @method('put')
-                            @else
-                                @method('post')
-                            @endif
+            @csrf
+            @if(isset($config))
+                @method('put')
+                @else
+                @method('post')
+            @endif
 
-                            <div class="row">
+            <div class="row">
 
-                                <div class="col-6">
+                    <div class="col-6">
 
-                                    @php
-                                        $tokenLengthList = Helper::tokenLengthList();
-                                    @endphp
+                        @php
+                            $tokenLengthList = Helper::tokenLengthList();
+                        @endphp
 
 
-                                    <div class="col-6">
-                                        <div class="form-group">
-                                            <label for="name" class="required">Token Length:</label>
-                                            <select name="token_length_string" id="token_length_string"
-                                                    class="browser-default custom-select token">
-                                                @foreach ($tokenLengthList as $key => $value)
-                                                    @if(isset($config))
-                                                        <option
-                                                            value="{{ $key }}" {{ ( $key == $config->token_length_string) ? 'selected' : '' }}>
-                                                    @else
-                                                        <option value="{{ $key }}">
-                                                            @endif
-                                                            {{ $value }}
-                                                        </option>
-                                                        @endforeach
-                                            </select>
-                                        </div>
-                                    </div>
+                        <div class="col-6">
+                            <div class="form-group">
+                                <label for="name" class="required">Token Length:</label>
+                                <select name="token_length_string" id="token_length_string"   class="browser-default custom-select token">
+                                    @foreach ($tokenLengthList as $key => $value)
+                                        @if(isset($config))
+                                            <option value="{{ $key }}" {{ ( $key == $config->token_length_string) ? 'selected' : '' }}>
+                                        @else
+                                            <option value="{{ $key }}">
+                                                @endif
+                                                {{ $value }}
+                                            </option>
+                                            @endforeach
+                                </select>
+                            </div>
+                        </div>
 
-                                    <input type="hidden" name="token_length_number" id="token_length_number">
+                        <input type="hidden" name="token_length_number" id="token_length_number">
 
-                                    <div class="col-6">
-                                        <div class="form-group">
-                                            <label for="code" class="required">Validation Time (Sec):</label>
-                                            <input required
-                                                   data-validation-required-message="Validation Time is required"
-                                                   name="validation_time"
-                                                   value="@if(isset($config)){{$config->validation_time}} @elseif(old("validation_time")) {{old("validation_time")}} @endif"
-                                                   type="text"
-                                                   class="form-control @error('current_version') is-invalid @enderror"
-                                                   placeholder="Enter Validation Time..">
-                                            <small
-                                                class="text-danger"> @error('validation_time') {{ $message }} @enderror </small>
-                                            <div class="help-block"></div>
-                                        </div>
-
-                                    </div>
-
-
-                                    <div class="col-5 mb-2">
-
-                                        <button type="submit" id="submitForm" style="width:100%"
-                                                class="btn @if(isset($config)) btn-success @else btn-info @endif ">
-                                            @if(isset($config)) Update OTP Config @else Create OTP Config @endif
-                                        </button>
-                                    </div>
-
-                                </div>
+                        <div class="col-6">
+                            <div class="form-group">
+                                <label for="code" class="required">Validation Time (Sec):</label>
+                                <input required data-validation-required-message="Validation Time is required"  name="validation_time"
+                                       value="@if(isset($config)){{$config->validation_time}} @elseif(old("validation_time")) {{old("validation_time")}} @endif"
+                                       type="text"  class="form-control @error('current_version') is-invalid @enderror" placeholder="Enter Validation Time..">
+                                <small class="text-danger"> @error('validation_time') {{ $message }} @enderror </small>
+                                <div class="help-block"></div>
                             </div>
 
-                        </form>
-        </div>
+                        </div>
 
+
+                        <div class="col-5 mb-2" >
+
+                        <button type="submit" id="submitForm" style="width:100%" class="btn @if(isset($config)) btn-success @else btn-info @endif ">
+                            @if(isset($config)) Update OTP Config @else Create OTP Config @endif
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+
+        </form>
     </div>
+
+</div>
 
 
 @endsection
@@ -128,8 +118,8 @@
 
         $('#token_length_number').val(token_string);
 
-        $(document).ready(function () {
-            $("#token_length_string").change(function () {
+        $(document).ready(function(){
+            $("#token_length_string").change(function(){
                 var token = $(this).children("option:selected").text();
                 console.log(token);
                 console.log("second");
