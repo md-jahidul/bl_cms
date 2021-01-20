88703fe4a3e879d0c005ddc279421b092bb6677b
masattar-bs <abdus.sattar@bs-23.net>
2021-01-20 15:09:22 +0600

    Add Banner purchase log models and migrations

---
+++ database/migrations/2021_01_20_145417_create_banner_product_purchases_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBannerProductPurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banner_product_purchases', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('banner_id');
+            $table->string('product_code');
+            $table->integer('total_buy')->default(0);
+            $table->integer('total_buy_attempt')->default(0);
+            $table->integer('total_cancel')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banner_product_purchases');
+    }
+}
