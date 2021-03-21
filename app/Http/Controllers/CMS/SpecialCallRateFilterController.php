7e545d7823dba36310efa744b539538847998ccc
Ahsan Habib <ahabib@bs-23.net>
2021-03-21 17:44:03 +0600

    special call filtaring plane has been done .

---
+++ app/Http/Controllers/CMS/SpecialCallRateFilterController.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Http\Controllers\CMS;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Services\InternetPackFilterService;
+use App\Services\MinutePackFilterService;
+use App\Services\MixedBundleFilterService;
+use App\Services\SpecialCallRateFilterService;
+use Illuminate\Support\Facades\Validator;
+
+class SpecialCallRateFilterController extends Controller
+{
+    protected $service;
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->service = new SpecialCallRateFilterService();
+    }
+
+    public function create()
+    {
+        $existing_sort_filters = $this->service->getAll()->sort()->active()->get();
+
+        $sort_filters = [];
+        foreach ($existing_sort_filters as $item) {
+            $filters = json_decode($item->filter, true);
+            $sort_filters [] = $filters['value'];
+        }
+        return view('admin.offer-special.config.create', compact('sort_filters'));
+    }
+
+    public function getPriceFilter(Request $request)
+    {
+        $builder = $this->service->getAll()->price()->active();
+
+        return $this->service->preparePriceFilterForDatatable($builder, $request);
+    }
+
+
+    public function getMinuteFilter(Request $request)
+    {
+        $builder = $this->service->getAll()->minute()->active();
+
+        return $this->service->preparePriceFilterForDatatable($builder, $request);
+    }
+
+    public function getValidityFilter(Request $request)
+    {
+        $builder = $this->service->getAll()->validity()->active();
+
+        return $this->service->preparePriceFilterForDatatable($builder, $request);
+    }
+
+    public function savePriceFilter(Request $request)
+    {
+        $validate = Validator::make(
+            $request->all(),
+            [
+                'lower' => 'required|numeric|max:2000',
+                'upper' => 'numeric|gt:lower|max:2000'
+            ]
+        );
+
+        if ($validate->fails()) {
+            $response = [
+                'success' => 'FAILED',
+                'errors' => $validate->errors()->first()
+            ];
+            return response()->json($response, 422);
+        }
+
+        return $this->service->addFilter($request, 'price', 'tk.');
+    }
+
+    public function deleteFilter(Request $request)
+    {
+        $validate = Validator::make(
+            $request->all(),
+            [
+                'id' => 'required|exists:minutes_pack_filters,id'
+            ]
+        );
+
+        if ($validate->fails()) {
+            $response = [
+                'success' => 'FAILED',
+                'errors' => $validate->errors()->first()
+            ];
+            return response()->json($response, 422);
+        }
+
+        return $this->service->delFilter($request);
+    }
+
+    public function saveMinuteFilter(Request $request)
+    {
+        $validate = Validator::make(
+            $request->all(),
+            [
+                'lower' => 'required|numeric|max:2000',
+                'upper' => 'numeric|gt:lower|max:2000'
+            ]
+        );
+
+        if ($validate->fails()) {
+            $response = [
+                'success' => 'FAILED',
+                'errors' => $validate->errors()->first()
+            ];
+            return response()->json($response, 422);
+        }
+
+        return $this->service->addFilter($request, 'minutes', 'minutes');
+    }
+
+
+    public function saveValidityFilter(Request $request)
+    {
+        $validate = Validator::make(
+            $request->all(),
+            [
+                'lower' => 'required|numeric|max:365',
+                'upper' => 'numeric|gt:lower|max:365'
+            ]
+        );
+
+        if ($validate->fails()) {
+            $response = [
+                'success' => 'FAILED',
+                'errors' => $validate->errors()->first()
+            ];
+            return response()->json($response, 422);
+        }
+
+        return $this->service->addFilter($request, 'validation', 'days');
+    }
+
+    public function saveSortFilter(Request $request)
+    {
+        $validate = Validator::make(
+            $request->all(),
+            [
+                'filters' => 'required|array',
+            ]
+        );
+
+        if ($validate->fails()) {
+            $response = [
+                'success' => 'FAILED',
+                'errors' => $validate->errors()->first()
+            ];
+            return response()->json($response, 422);
+        }
+
+        return $this->service->addSortFilter($request);
+    }
+}
---
+++ app/Services/SpecialCallRateFilterService.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\InternetPackFilter;
+use App\Models\SpecialCallRateFilter;
+use App\Models\MixedBundleFilter;
+use App\Models\OfferFilterType;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Class MinutePackFilterService
+ * @package App\Services
+ */
+class SpecialCallRateFilterService
+{
+    protected $filter_types = [];
+
+    public function __construct()
+    {
+        $this->filter_types = OfferFilterType::all()->pluck('id', 'slug');
+    }
+
+    public function getAll()
+    {
+        return new SpecialCallRateFilter();
+    }
+
+    /**
+     * @param  Builder  $itemBuilder
+     * @param  Request  $request
+     * @return array
+     */
+    public function preparePriceFilterForDatatable(Builder $itemBuilder, Request $request)
+    {
+        $draw = $request->draw;
+        $all_items_count = $itemBuilder->count();
+        $items = $itemBuilder->get();
+
+        $response = [
+            'draw'  =>  $draw,
+            'recordsTotal'  =>  $all_items_count,
+            'recordsFiltered'  =>  $all_items_count,
+            'data'  =>  []
+        ];
+
+        $items->each(function ($item) use (&$response) {
+
+            $filter = json_decode($item->filter, true);
+
+            $response['data'][] = [
+                'id'      =>  $item->id,
+                'lower'   =>  $filter['lower'],
+                'upper'   =>  $filter['upper'],
+                'unit'   =>  $filter['unit']
+            ];
+        });
+
+        $collection = collect($response['data']);
+
+        $sorted = $collection->sortBy('lower');
+
+        $response['data'] = $sorted->values()->all();
+        return $response;
+    }
+
+    public function addFilter(Request $request, $type, $unit)
+    {
+        $lower = $request->lower;
+        $upper = $request->upper;
+
+        try {
+            SpecialCallRateFilter::create([
+                'offer_filter_type_id' => $this->filter_types[$type],
+                'filter' => json_encode([
+                    'lower' => $lower,
+                    'upper' => $upper,
+                    'unit'  => $unit
+                ]),
+            ]);
+
+            $response = [
+                'status' => 'SUCCESS',
+                'message' => 'Filter Added Successfully'
+            ];
+
+            return response()->json($response, 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => ' FAILED',
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function delFilter(Request $request)
+    {
+        try {
+            $filter = SpecialCallRateFilter::find($request->id);
+            $filter->delete();
+            $response = [
+                'status' => 'SUCCESS',
+                'message' => 'Filter deleted Successfully'
+            ];
+
+            return response()->json($response, 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => ' FAILED',
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+
+    public function addSortFilter(Request $request)
+    {
+        DB::beginTransaction();
+
+        try {
+            // first delete previous data and save new data. Done with transaction.if fails revert operation
+            DB::table('minutes_pack_filters')
+                ->where('offer_filter_type_id', $this->filter_types['sort'])
+                ->delete();
+
+            //save
+            $filters = $request->filters;
+            foreach ($filters as $filter) {
+                SpecialCallRateFilter::create([
+                    'offer_filter_type_id' => $this->filter_types['sort'],
+                    'filter' => json_encode([
+                        'name'  => $filter['name'],
+                        'value' => $filter['value']
+                    ]),
+                ]);
+            }
+            $response = [
+                'status' => 'SUCCESS',
+                'message' => 'Filter Added Successfully'
+            ];
+
+            DB::commit();
+
+            return response()->json($response, 200);
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            return response()->json([
+                'status' => ' FAILED',
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
+}
