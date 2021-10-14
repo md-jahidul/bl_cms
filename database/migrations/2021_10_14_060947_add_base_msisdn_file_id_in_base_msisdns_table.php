b8080caabd65353d734f3e1962edcdaeacec46cd
jahidul <jahidul@bs-23.net>
2021-10-14 14:55:54 +0600

    made migration file for base msisdn file

---
+++ database/migrations/2021_10_14_060947_add_base_msisdn_file_id_in_base_msisdns_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBaseMsisdnFileIdInBaseMsisdnsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('base_msisdns', function (Blueprint $table) {
+            $table->unsignedBigInteger('base_msisdn_file_id')
+                ->nullable()
+                ->after('group_id');
+            $table->foreign('base_msisdn_file_id')
+                ->references('id')
+                ->on('base_msisdns')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('base_msisdns', function (Blueprint $table) {
+            //
+        });
+    }
+}
