455b1b77363831e884dc3de932bc2f522c367b95
HabiburRaju <habibur.rahaman@brainstation-23.com>
2021-12-01 12:39:44 +0600

    Fix : Notification Schedul Date Format

--- app/Http/Controllers/CMS/NotificationV2/NotificationV2Controller.php
+++ app/Http/Controllers/CMS/NotificationV2/NotificationV2Controller.php
@@ -288,6 +288,7 @@ class NotificationV2Controller extends Controller
         $temp = Storage::disk('excel_uploads')->put($path, $request->customer_file);
         $file_name = 'public/uploads/'.$temp;
         $time = (explode("-",$request->schedule_time));
+        $time[0] = date("Y-m-d H:i:s", strtotime("+0 minutes",strtotime($time[0])));
         $time[1] = date("Y-m-d H:i:s", strtotime("+2 minutes",strtotime($time[1])));
         
         $this->notificationV2Service->createNotificationSchedule($request->all(), $file_name, $time);
