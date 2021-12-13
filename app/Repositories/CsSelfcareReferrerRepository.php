d7423b3d3c59f0955977c3164414862cbca8a759
najmusbs23 <najmus.sakib@brainstation-23.com>
2021-12-13 15:29:19 +0600

    Refactor code in Retailer Script and RAFM report

--- app/Repositories/CsSelfcareReferrerRepository.php
+++ app/Repositories/CsSelfcareReferrerRepository.php
@@ -7,4 +7,10 @@ use App\Models\CsSelfcareReferrer;
 class CsSelfcareReferrerRepository extends BaseRepository
 {
     protected $modelName = CsSelfcareReferrer::class;
+
+    public function checkRecord($msisdn)
+    {
+        $msisdn = "0" . $msisdn;
+        return $this->model->select('id')->where('referrer', $msisdn)->first();
+    }
 }
