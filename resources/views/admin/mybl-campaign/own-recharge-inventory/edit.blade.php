374d8a0b9703e231c5f9bdacc53745fb114e33b6
HabiburRaju <habibur.rahaman@brainstation-23.com>
2022-04-20 11:19:18 +0600

    Design Issue Fix

--- resources/views/admin/mybl-campaign/own-recharge-inventory/edit.blade.php
+++ resources/views/admin/mybl-campaign/own-recharge-inventory/edit.blade.php
@@ -324,25 +324,6 @@
                                 </div>
 
                                 <!-- Product Selection End -->
-
-                                <div class="row">
-                                    <div class="form-actions col-md-12 mt-0 text-danger"></div>
-
-                                    <div class="form-group col-md-6 mb-2">
-                                        <label for="status_input">Campaign Status: </label>
-                                        <div class="form-group {{ $errors->has('status') ? ' error' : '' }}">
-                                            <input type="radio" name="status" value="1" id="input-radio-15"
-                                                {{ (isset($campaign->status) && $campaign->status == 1) ? 'checked' : '' }}>
-                                            <label for="input-radio-15" class="mr-3">Active</label>
-                                            <input type="radio" name="status" value="0" id="input-radio-16"
-                                                {{ (isset($campaign->status) && $campaign->status == 0) ? 'checked' : '' }}
-                                                {{ isset($campaign->status) ? '' : 'checked' }}>
-                                            <label for="input-radio-16" class="mr-3">Inactive</label>
-                                            @if ($errors->has('status'))
-                                                <div class="help-block">  {{ $errors->first('status') }}</div>
-                                            @endif
-                                        </div>
-                                    </div>
                                      {{-- WINNING LOGIC & CAPPING --}}
                                     <div class="row">
                                         <input type="hidden" name="winning_capping_id" value={{ $winningCampaignLogics->id }}>
@@ -353,7 +334,7 @@
                                             <div class="form-group {{ $errors->has('winning_type') ? ' error' : '' }}">
                                                 <input type="radio" name="winning_type" value="first_recharge" id="input-radio-15"
                                                 {{ (isset($winningCampaignLogics->winning_type) && $winningCampaignLogics->winning_type == "first_recharge") ? 'checked' : '' }}>
-                                                <label for="input-radio-15" class="mr-3">First Recharge/Purchase</label>
+                                                <label for="input-radio-15" class="mr-3">First Recharge/Purchase</label> <br>
                                                 <input type="radio" name="winning_type" value="highest_recharge" id="input-radio-16"
                                                 {{ (isset($winningCampaignLogics->winning_type) && $winningCampaignLogics->winning_type == "highest_recharge") ? 'checked' : '' }}>
                                                 <label for="input-radio-16" class="mr-3">Highest Recharge/Purchase</label>
@@ -432,6 +413,24 @@
                                             <small class="text-danger"> @error('communication_message_bn') {{ $message }} @enderror </small>
                                         </div>
                                     </div>
+                                    <div class="row">
+                                        <div class="form-actions col-md-12 mt-0 text-danger"></div>
+    
+                                        <div class="form-group col-md-6 mb-2">
+                                            <label for="status_input">Campaign Status: </label>
+                                            <div class="form-group {{ $errors->has('status') ? ' error' : '' }}">
+                                                <input type="radio" name="status" value="1" id="input-radio-15"
+                                                    {{ (isset($campaign->status) && $campaign->status == 1) ? 'checked' : '' }}>
+                                                <label for="input-radio-15" class="mr-3">Active</label>
+                                                <input type="radio" name="status" value="0" id="input-radio-16"
+                                                    {{ (isset($campaign->status) && $campaign->status == 0) ? 'checked' : '' }}
+                                                    {{ isset($campaign->status) ? '' : 'checked' }}>
+                                                <label for="input-radio-16" class="mr-3">Inactive</label>
+                                                @if ($errors->has('status'))
+                                                    <div class="help-block">  {{ $errors->first('status') }}</div>
+                                                @endif
+                                            </div>
+                                        </div>
                                     <div class="form-actions col-md-12">
                                         <div class="pull-right">
                                             <button id="save" class="btn btn-primary"><i
