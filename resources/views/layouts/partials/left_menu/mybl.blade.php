e69857dd88ec6bda02d2bd87104b036abc995fb7
HabiburRaju <habibur.rahaman@brainstation-23.com>
2022-04-20 10:11:43 +0600

    Merge Conflict Resolved

--- resources/views/layouts/partials/left_menu/mybl.blade.php
+++ resources/views/layouts/partials/left_menu/mybl.blade.php
@@ -319,12 +319,8 @@
                 <a class="menu-item" href="{{ route('mybl-campaign.index') }} "
                    data-i18n="nav.templates.vert.classic_menu">
                     <i class="la la-bullhorn"></i> Mybl Campaign
-            {{-- <li class="{{is_active_url('own-recharge-inventory')}}">
-                <a class="menu-item" href="{{ route('own-recharge-inventory.index') }} "
-                   data-i18n="nav.templates.vert.classic_menu">
-                    <i class="la la-dollar"></i> Own Recharge Inventory
                 </a>
-            </li> --}}
+            </li>
         </ul>
     </li>
         <!-- Campaign Menu -->
