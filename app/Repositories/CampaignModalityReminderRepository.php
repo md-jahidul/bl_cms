ff3f34c8635e0a3ed907f665149070704a9b6ad5
HabiburRaju <habibur.rahaman@brainstation-23.com>
2022-07-17 16:19:58 +0600

    Campaign Modality Reminder added

---
+++ app/Repositories/CampaignModalityReminderRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\CampaignModalityRemainder;
+use App\Models\Referee;
+use Carbon\Carbon;
+
+class CampaignModalityReminderRepository extends BaseRepository
+{
+    public $modelName = CampaignModalityRemainder::class;
+}
