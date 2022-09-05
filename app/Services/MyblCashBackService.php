0a3bba637f19448305d25b6807d722b8b129ac34
HabiburRaju <habibur.rahaman@brainstation-23.com>
2022-09-05 15:16:25 +0600

    Cashback edit issue fix

--- app/Services/MyblCashBackService.php
+++ app/Services/MyblCashBackService.php
@@ -48,6 +48,9 @@ class MyblCashBackService
                 if(isset($product['override_other_campaign'])) {
                     $product['override_other_campaign'] = 1;
                 }
+
+                $product['start_date'] = ($product['start_date'] == null ? $data['start_date'] : $product['start_date']);
+                $product['end_date'] = ($product['end_date'] == null ? $data['end_date'] : $product['end_date']);
                 $product['mybl_cash_back_id'] = $campaign->id;
                 $product['status'] = isset($product['status']) ?? 0;
                 $this->cashBackProductRepo->save($product);
@@ -74,6 +77,10 @@ class MyblCashBackService
                 else {
                     $product['override_other_campaign'] = 0;
                 }
+
+                $product['start_date'] = ($product['start_date'] == null ? $data['start_date'] : $product['start_date']);
+                $product['end_date'] = ($product['end_date'] == null ? $data['end_date'] : $product['end_date']);
+
                 $product['mybl_cash_back_id'] = $id;
                 $this->cashBackProductRepo->save($product);
             }
