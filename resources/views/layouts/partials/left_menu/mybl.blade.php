a799bc7b10b8def8d8c1292d9df9d99fcfe222ba
HabiburRaju <habibur.rahaman@brainstation-23.com>
2022-10-13 12:35:31 +0600

    FIx: Duplicate option removed

--- resources/views/layouts/partials/left_menu/mybl.blade.php
+++ resources/views/layouts/partials/left_menu/mybl.blade.php
@@ -97,13 +97,6 @@
                 </li>
             </ul>
             <ul class="menu-content">
-                <li class=" {{is_active_url(route('heme-navigation-rail.index'))}}">
-                    <a class="menu-item" href="{{ route('heme-navigation-rail.index') }}">
-                        <i class="la la-paper-plane"></i>Home Navigation Rail
-                    </a>
-                </li>
-            </ul>
-            <ul class="menu-content">
                 <li class=" {{is_active_url(route('content-navigation-rail.index'))}}">
                     <a class="menu-item" href="{{ route('content-navigation-rail.index') }}">
                         <i class="la la-paper-plane"></i>Content Navigation Rail
