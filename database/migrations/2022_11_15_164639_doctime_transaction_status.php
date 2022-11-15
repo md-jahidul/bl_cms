6768d2f972ce928191071e2f44ff45db504756ea
abusabir_BL <abu.sabir@annanovas.com>
2022-11-15 17:05:03 +0600

    post-transaction-status table migration create

---
+++ database/migrations/2022_11_15_164639_doctime_transaction_status.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DoctimeTransactionStatus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('post-transaction-status', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('contact_no')->index();
+            $table->enum('service', ['consultation', 'subscription_purchase', 'diagnostic_order','medicine_order']);
+            $table->unsignedBigInteger('service_id');
+            $table->float('amount',15,2);
+            $table->timestamp('transaction_time');
+            $table->string('transaction_id');
+            $table->text('remarks')->nullable();
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
