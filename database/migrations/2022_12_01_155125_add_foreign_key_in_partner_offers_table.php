c20b2439f2041906bb53c9464a93668645b5c994
Jahidul Islam <jahidul@brainstation-23.com>
2022-12-04 17:21:44 +0600

    Issu fix migration file

--- database/migrations/2022_12_01_155125_add_foreign_key_in_partner_offers_table.php
+++ database/migrations/2022_12_01_155125_add_foreign_key_in_partner_offers_table.php
@@ -15,9 +15,9 @@ class AddForeignKeyInPartnerOffersTable extends Migration
     {
         Schema::table('partner_offers', function (Blueprint $table) {
             $table->string('card_img')->after('product_code')->nullable();
-            $table->string('card_img_en')->after('card_img')->nullable();
-            $table->string('card_img_bn')->after('card_img_en')->nullable();
-            $table->string('card_alt_text_en')->after('card_img_bn')->nullable();
+            $table->string('card_img_name_en')->after('card_img')->nullable();
+            $table->string('card_img_name_bn')->after('card_img_name_en')->nullable();
+            $table->string('card_alt_text_en')->after('card_img_name_bn')->nullable();
             $table->string('card_alt_text_bn')->after('card_alt_text_en')->nullable();
             $table->unsignedBigInteger('loyalty_tier_id')->after('partner_id')->nullable();
             $table->unsignedBigInteger('partner_category_id')->after('partner_id')->nullable();
@@ -33,6 +33,10 @@ class AddForeignKeyInPartnerOffersTable extends Migration
     {
         Schema::table('partner_offers', function (Blueprint $table) {
             $table->dropColumn('card_img');
+            $table->dropColumn('card_img_name_en');
+            $table->dropColumn('card_img_name_bn');
+            $table->dropColumn('card_alt_text_en');
+            $table->dropColumn('card_alt_text_bn');
             $table->dropColumn('loyalty_tier_id');
             $table->dropColumn('partner_category_id');
         });
