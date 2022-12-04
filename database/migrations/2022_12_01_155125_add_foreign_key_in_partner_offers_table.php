d9fbd64e491db973415524bc3483e5f5e37ca2cc
Jahidul Islam <jahidul@brainstation-23.com>
2022-12-04 17:15:40 +0600

    Issu fix migration file

--- database/migrations/2022_12_01_155125_add_foreign_key_in_partner_offers_table.php
+++ database/migrations/2022_12_01_155125_add_foreign_key_in_partner_offers_table.php
@@ -17,8 +17,8 @@ class AddForeignKeyInPartnerOffersTable extends Migration
             $table->string('card_img')->after('product_code')->nullable();
             $table->string('card_img_en')->after('card_img')->nullable();
             $table->string('card_img_bn')->after('card_img_en')->nullable();
-            $table->string('alt_text_en')->after('card_img_bn')->nullable();
-            $table->string('alt_text_bn')->after('alt_text_en')->nullable();
+            $table->string('card_alt_text_en')->after('card_img_bn')->nullable();
+            $table->string('card_alt_text_bn')->after('card_alt_text_en')->nullable();
             $table->unsignedBigInteger('loyalty_tier_id')->after('partner_id')->nullable();
             $table->unsignedBigInteger('partner_category_id')->after('partner_id')->nullable();
         });
