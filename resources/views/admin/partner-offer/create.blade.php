930e84ab61ba5fff1f255b4bcc6fa42802ae74ef
Jahidul Islam <jahidul@brainstation-23.com>
2022-12-28 20:09:43 +0600

    Partner Offer create and edit cat and tier add

--- resources/views/admin/partner-offer/create.blade.php
+++ resources/views/admin/partner-offer/create.blade.php
@@ -35,13 +35,43 @@
                                         required data-validation-required-message="Please select area">
                                     <option value="">---Select Offer Area---</option>
                                     @foreach($areas as $val)
-                                    <option value="{{$val->id}}">{{$val->area_en}}</option>
+                                        <option value="{{$val->id}}">{{$val->area_en}}</option>
                                     @endforeach
-
                                 </select>
                                 <div class="help-block"></div>
                                 @if ($errors->has('area_id'))
                                 <div class="help-block">{{ $errors->first('area_id') }}</div>
+                                @endif
+                            </div>
+
+                            <div class="form-group col-md-4 col-xs-12 {{ $errors->has('partner_category_id') ? ' error' : '' }}">
+                                <label for="offer_unit" class="required">Categories</label>
+                                <select class="form-control required" name="partner_category_id"
+                                        required data-validation-required-message="Please select area">
+                                    <option value="">---Select Categories---</option>
+                                    @foreach($categories as $val)
+                                        <option value="{{$val->id}}">{{$val->name_en}}</option>
+                                    @endforeach
+                                </select>
+                                <div class="help-block"></div>
+                                @if ($errors->has('partner_category_id'))
+                                    <div class="help-block">{{ $errors->first('partner_category_id') }}</div>
+                                @endif
+                            </div>
+
+
+                            <div class="form-group col-md-4 col-xs-12 {{ $errors->has('loyalty_tier_id') ? ' error' : '' }}">
+                                <label for="offer_unit" class="required">Tier</label>
+                                <select class="form-control required" name="loyalty_tier_id"
+                                        required data-validation-required-message="Please select area">
+                                    <option value="">---Select Tier---</option>
+                                    @foreach($tiers as $val)
+                                        <option value="{{$val->id}}">{{$val->title_en}}</option>
+                                    @endforeach
+                                </select>
+                                <div class="help-block"></div>
+                                @if ($errors->has('loyalty_tier_id'))
+                                    <div class="help-block">{{ $errors->first('loyalty_tier_id') }}</div>
                                 @endif
                             </div>
 
