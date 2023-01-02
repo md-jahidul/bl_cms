628d76bf4a3557e2f30e0780a982cf82baabf974
fahreyad <fahreyad.hossain@brainstation-23.com>
2023-01-02 17:00:11 +0600

    cms update

---
+++ app/Repositories/SubFooterRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * User: Md Fahreyad Hossain
+ * Date: 1/1/2023
+ */
+
+namespace App\Repositories;
+
+
+use App\Models\SubFooter;
+
+class SubFooterRepository extends BaseRepository {
+
+    public $modelName = SubFooter::class;
+
+}
