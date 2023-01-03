f5f7a86aa9c69d000629b36357463f1f127c579f
Jahidul Islam <jahidul@brainstation-23.com>
2023-01-03 17:53:05 +0600

    Add details btn and tag in blog

--- database/migrations/2022_12_27_171313_add_additional_field_in_media_press_news_events_table.php
+++ database/migrations/2022_12_27_171313_add_additional_field_in_media_press_news_events_table.php
@@ -18,6 +18,10 @@ class AddAdditionalFieldInMediaPressNewsEventsTable extends Migration
             $table->integer('reference_id')->nullable()->after('reference_type');
             $table->tinyInteger('show_in_home')->default(0)->after('status');
             $table->integer('read_time')->default(0)->after('date');
+            $table->string('details_btn_en')->nullable()->after('details_image_name_bn');
+            $table->string('details_btn_bn')->nullable()->after('details_btn_en');
+            $table->string('tag_en')->nullable()->after('alt_text_bn');
+            $table->string('tag_bn')->nullable()->after('tag_en');
         });
     }
 
@@ -33,6 +37,10 @@ class AddAdditionalFieldInMediaPressNewsEventsTable extends Migration
             $table->dropColumn('reference_id');
             $table->dropColumn('show_in_home');
             $table->dropColumn('read_time');
+            $table->dropColumn('details_btn_en');
+            $table->dropColumn('details_btn_bn');
+            $table->dropColumn('tag_en');
+            $table->dropColumn('tag_bn');
         });
     }
 }
--- resources/views/admin/blog/post/edit.blade.php
+++ resources/views/admin/blog/post/edit.blade.php
@@ -73,17 +73,6 @@
                                     @endif
                                 </div>
 
-{{--                                <div class="form-group col-md-6 {{ $errors->has('alt_text_en') ? ' error' : '' }}">--}}
-{{--                                    <label for="alt_text_en" class="">Alt Text</label>--}}
-{{--                                    <input type="text" id="alt_text_en" name="alt_text_en" class="form-control" placeholder="Enter alt text"--}}
-{{--                                           value="{{ $blogPost->alt_text_en }}"--}}
-{{--                                           required data-validation-required-message="Enter alt text">--}}
-{{--                                    <div class="help-block"></div>--}}
-{{--                                    @if ($errors->has('alt_text_en'))--}}
-{{--                                        <div class="help-block">  {{ $errors->first('alt_text_en') }}</div>--}}
-{{--                                    @endif--}}
-{{--                                </div>--}}
-
                                 <div class="form-group col-md-6 {{ $errors->has('short_details_en') ? ' error' : '' }}">
                                     <label for="short_details_en" class="required">Short Description En</label>
                                     <textarea type="text" name="short_details_en"  class="form-control" placeholder="Enter short description in English" required rows="3"
@@ -104,51 +93,45 @@
                                     @endif
                                 </div>
 
-{{--                                <slot id="press_release" data-offer-type="press_release" class="{{ $blogPost->type == "press_release" ? '' : 'd-none' }}">--}}
-{{--                                    <h5><strong>Pop Up Section</strong></h5>--}}
-{{--                                    <div class="form-actions col-md-12 mt-0"></div>--}}
+                                <div class="form-group col-md-3 {{ $errors->has('details_btn_en') ? ' error' : '' }}">
+                                    <label for="details_btn_en" class="required">Details Button En</label>
+                                    <input type="text" name="details_btn_en"  class="form-control" placeholder="Enter title in English"
+                                           value="{{ $blogPost->details_btn_en }}" required data-validation-required-message="Enter title in English">
+                                    <div class="help-block"></div>
+                                    @if ($errors->has('details_btn_en'))
+                                        <div class="help-block">  {{ $errors->first('details_btn_en') }}</div>
+                                    @endif
+                                </div>
 
-{{--                                    <div class="form-group col-md-6 {{ $errors->has('details_image') ? ' error' : '' }}">--}}
-{{--                                        <label for="details_image" class="required">Pop Up Banner Image</label>--}}
-{{--                                        <input type="file" name="details_image" class="form-control dropify" data-height="90"--}}
-{{--                                               data-default-file="{{ config('filesystems.file_base_url') . $blogPost->details_image }}"--}}
-{{--                                               value="{{ old("details_image") ? old("details_image") : '' }}">--}}
-{{--                                        <div class="help-block"></div>--}}
-{{--                                        @if ($errors->has('details_image'))--}}
-{{--                                            <div class="help-block">  {{ $errors->first('details_image') }}</div>--}}
-{{--                                        @endif--}}
-{{--                                    </div>--}}
+                                <div class="form-group col-md-3 {{ $errors->has('details_btn_bn') ? ' error' : '' }}">
+                                    <label for="details_btn_bn" class="required">Details Button Bn</label>
+                                    <input type="text" name="details_btn_bn"  class="form-control" placeholder="Enter button label in Bangla"
+                                           value="{{ $blogPost->details_btn_bn }}" required data-validation-required-message="Enter title in Bangla">
+                                    <div class="help-block"></div>
+                                    @if ($errors->has('details_btn_bn'))
+                                        <div class="help-block">  {{ $errors->first('details_btn_bn') }}</div>
+                                    @endif
+                                </div>
 
-{{--                                    <div class="form-group col-md-6 {{ $errors->has('details_alt_text_en') ? ' error' : '' }}">--}}
-{{--                                        <label for="details_alt_text_en" class="">Alt Text</label>--}}
-{{--                                        <input type="text" id="details_alt_text_en" name="details_alt_text_en" class="form-control" placeholder="Enter alt text"--}}
-{{--                                               value="{{ $blogPost->details_alt_text_en }}">--}}
-{{--                                        <div class="help-block"></div>--}}
-{{--                                        @if ($errors->has('details_alt_text_en'))--}}
-{{--                                            <div class="help-block">  {{ $errors->first('details_alt_text_en') }}</div>--}}
-{{--                                        @endif--}}
-{{--                                    </div>--}}
+                                <div class="form-group col-md-3 {{ $errors->has('tag_en') ? ' error' : '' }}">
+                                    <label for="tag_en">Tag En</label>
+                                    <input type="text" name="tag_en"  class="form-control" placeholder="Enter tag in English"
+                                           value="{{ $blogPost->tag_en }}" required data-validation-required-message="Enter button label in English">
+                                    <div class="help-block"></div>
+                                    @if ($errors->has('tag_en'))
+                                        <div class="help-block">  {{ $errors->first('tag_en') }}</div>
+                                    @endif
+                                </div>
 
-{{--                                    <div class="form-group col-md-6 {{ $errors->has('long_details_bn') ? ' error' : '' }}">--}}
-{{--                                        <label for="long_details_bn" class="required">Long Description En</label>--}}
-{{--                                        <textarea type="text" name="long_details_en" class="form-control summernote_editor" placeholder="Enter long description in English" required--}}
-{{--                                                  data-validation-required-message="Enter long description in English">{{ $blogPost->long_details_en }}</textarea>--}}
-{{--                                        <div class="help-block"></div>--}}
-{{--                                        @if ($errors->has('long_details_bn'))--}}
-{{--                                            <div class="help-block">  {{ $errors->first('long_details_bn') }}</div>--}}
-{{--                                        @endif--}}
-{{--                                    </div>--}}
-
-{{--                                    <div class="form-group col-md-6 {{ $errors->has('long_details_bn') ? ' error' : '' }}">--}}
-{{--                                        <label for="long_details_bn" class="required">Long Description BN</label>--}}
-{{--                                        <textarea type="text" name="long_details_bn"  class="form-control summernote_editor" placeholder="Enter long description in Bangla" required--}}
-{{--                                                  data-validation-required-message="Enter long description in Bangla">{{ $blogPost->long_details_bn }}</textarea>--}}
-{{--                                        <div class="help-block"></div>--}}
-{{--                                        @if ($errors->has('long_details_bn'))--}}
-{{--                                            <div class="help-block">  {{ $errors->first('long_details_bn') }}</div>--}}
-{{--                                        @endif--}}
-{{--                                    </div>--}}
-{{--                                </slot>--}}
+                                <div class="form-group col-md-3 {{ $errors->has('tag_bn') ? ' error' : '' }}">
+                                    <label for="tag_bn">Tag Bn</label>
+                                    <input type="text" name="tag_bn"  class="form-control" placeholder="Enter tag in Bangla"
+                                           value="{{ $blogPost->tag_bn }}" required data-validation-required-message="Enter title in Bangla">
+                                    <div class="help-block"></div>
+                                    @if ($errors->has('tag_bn'))
+                                        <div class="help-block">  {{ $errors->first('tag_bn') }}</div>
+                                    @endif
+                                </div>
 
                                 <div class="col-md-6">
                                     <div class="form-group" id="show_in_home">
