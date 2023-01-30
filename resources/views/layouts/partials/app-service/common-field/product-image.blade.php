5b10609ae78fd02dcb8e7fd233c94dafa55efd6f
Jahidul Islam <jahidul@brainstation-23.com>
2023-01-30 15:08:00 +0600

    Product image add in app service

--- resources/views/layouts/partials/app-service/common-field/product-image.blade.php
+++ resources/views/layouts/partials/app-service/common-field/product-image.blade.php
@@ -1,8 +1,9 @@
-<div class="form-group col-md-5 {{ $errors->has('product_img_url') ? ' error' : '' }}">
+<div class="form-group col-md-6 {{ $errors->has('product_img_url') ? ' error' : '' }}">
     <label for="alt_text">Product Image</label>
     <div class="custom-file">
-        <input type="file" name="product_img_url" class="custom-file-input" id="{{ $imgField }}">
-        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+        <input type="file" name="product_img_url" class="custom-file-input dropify" id="{{ $imgField }}"
+               data-default-file="{{ isset($appServiceProduct->product_img_url) ? config('filesystems.file_base_url') . $appServiceProduct->product_img_url : '' }}">
+{{--        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>--}}
     </div>
     <span class="text-primary">Please given file type (.png, .jpg)</span>
 
@@ -12,11 +13,11 @@
     @endif
 </div>
 
-<div class="form-group col-md-1">
-    @if(isset($appServiceProduct->product_img_url))
-        <img src="{{ isset($appServiceProduct->product_img_url) ? config('filesystems.file_base_url') . $appServiceProduct->product_img_url : '' }}"
-         class="{{ isset($appServiceProduct->product_img_url) ? '' : 'd-none' }}" height="70" width="70" id="{{ $showImg }}">
-    @else
-        <img style="height:70px;width:70px;display:none" id="{{ $showImg }}">
-    @endif
-</div>
+{{--<div class="form-group col-md-1">--}}
+{{--    @if(isset($appServiceProduct->product_img_url))--}}
+{{--        <img src="{{ isset($appServiceProduct->product_img_url) ? config('filesystems.file_base_url') . $appServiceProduct->product_img_url : '' }}"--}}
+{{--         class="{{ isset($appServiceProduct->product_img_url) ? '' : 'd-none' }}" height="70" width="70" id="{{ $showImg }}">--}}
+{{--    @else--}}
+{{--        <img style="height:70px;width:70px;display:none" id="{{ $showImg }}">--}}
+{{--    @endif--}}
+{{--</div>--}}
