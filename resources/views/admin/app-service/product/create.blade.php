f4c3acd8afde760d570bef07b35492369c48a46c
Jahidul Islam <jahidul@brainstation-23.com>
2023-01-30 15:42:15 +0600

    Add product image in app service

--- resources/views/admin/app-service/product/create.blade.php
+++ resources/views/admin/app-service/product/create.blade.php
@@ -102,10 +102,24 @@
                                 @endif
                             </div>
 
+                            <div class="form-group col-md-12 {{ $errors->has('product_img_url') ? ' error' : '' }}">
+                                <label for="alt_text">Product Image</label>
+                                <div class="custom-file">
+                                    <input type="file" name="product_img_url" class="custom-file-input dropify"
+                                           data-default-file="{{ isset($appServiceProduct->product_img_url) ? config('filesystems.file_base_url') . $appServiceProduct->product_img_url : '' }}">
+                                </div>
+                                <span class="text-primary">Please given file type (.png, .jpg)</span>
 
+                                <div class="help-block"></div>
+                                @if ($errors->has('product_img_url'))
+                                    <div class="help-block">  {{ $errors->first('product_img_url') }}</div>
+                                @endif
+                            </div>
 
                             <slot id="app" data-offer-type="app" style="display: none">
                                 @include('layouts.partials.app-service.app')
+
+                                @include('layouts.partials.app-service.referral')
                             </slot>
 
                             <slot id="vas" data-offer-type="vas" style="display: none">
--- resources/views/layouts/partials/app-service/common-field/product-image.blade.php
+++ resources/views/layouts/partials/app-service/common-field/product-image.blade.php
@@ -1,17 +1,17 @@
-<div class="form-group col-md-6 {{ $errors->has('product_img_url') ? ' error' : '' }}">
-    <label for="alt_text">Product Image</label>
-    <div class="custom-file">
-        <input type="file" name="product_img_url" class="custom-file-input dropify" id="{{ $imgField }}"
-               data-default-file="{{ isset($appServiceProduct->product_img_url) ? config('filesystems.file_base_url') . $appServiceProduct->product_img_url : '' }}">
+{{--<div class="form-group col-md-6 {{ $errors->has('product_img_url') ? ' error' : '' }}">--}}
+{{--    <label for="alt_text">Product Image</label>--}}
+{{--    <div class="custom-file">--}}
+{{--        <input type="file" name="product_img_url" class="custom-file-input dropify" id="{{ $imgField }}"--}}
+{{--               data-default-file="{{ isset($appServiceProduct->product_img_url) ? config('filesystems.file_base_url') . $appServiceProduct->product_img_url : '' }}">--}}
 {{--        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>--}}
-    </div>
-    <span class="text-primary">Please given file type (.png, .jpg)</span>
+{{--    </div>--}}
+{{--    <span class="text-primary">Please given file type (.png, .jpg)</span>--}}
 
-    <div class="help-block"></div>
-    @if ($errors->has('product_img_url'))
-        <div class="help-block">  {{ $errors->first('product_img_url') }}</div>
-    @endif
-</div>
+{{--    <div class="help-block"></div>--}}
+{{--    @if ($errors->has('product_img_url'))--}}
+{{--        <div class="help-block">  {{ $errors->first('product_img_url') }}</div>--}}
+{{--    @endif--}}
+{{--</div>--}}
 
 {{--<div class="form-group col-md-1">--}}
 {{--    @if(isset($appServiceProduct->product_img_url))--}}
