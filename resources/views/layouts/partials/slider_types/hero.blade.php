a62e7421f94e134b9c1196fa5c7a87e2786227bd
fahreyad <fahreyad.hossain@brainstation-23.com>
2023-02-07 15:49:43 +0600

    home page issue fixed

--- resources/views/layouts/partials/slider_types/hero.blade.php
+++ resources/views/layouts/partials/slider_types/hero.blade.php
@@ -20,6 +20,7 @@
 
 @include('layouts.partials.slider_types.text_area')
 @include('layouts.partials.common_types.label_with_url')
+@include('layouts.partials.common_types.label_with_url',['count'=>1])
 @include('layouts.partials.slider_types.app_links')
 
 
