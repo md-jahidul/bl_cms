0336d7d3ef577ca3c7e282b3a0896f25bbcfe1c4
Mahir <Mahir.Shahrier@Brainstation-23.com>
2023-02-14 17:15:22 +0600

    migration updated

--- database/migrations/2023_02_14_144245_create_commerce_bill_statuses_table.php
+++ database/migrations/2023_02_14_144245_create_commerce_bill_statuses_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateCommerceBillStatusesTable extends Migration
 {
@@ -14,28 +15,28 @@ class CreateCommerceBillStatusesTable extends Migration
     public function up()
     {
         Schema::create('commerce_bill_statuses', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->string('result');
-            $table->string('message');
-            $table->string('bill_payment_id');
-            $table->string('bill_refer_id');
-            $table->string('bill_id');
-            $table->string('bill_name');
-            $table->string('bill_no');
-            $table->string('biller_acc_no');
-            $table->string('biller_mobile');
-            $table->string('bill_from');
-            $table->string('bill_to');
-            $table->string('bill_gen_date');
-            $table->string('bill_due_date');
-            $table->string('charge');
-            $table->string('bill_total_amount');
-            $table->string('transaction_id');
-            $table->string('payment_date');
-            $table->string('payment_status');
-            $table->string('payment_amount');
-            $table->string('payment_trx_id');
-            $table->string('payment_method');
+            $table->uuid('uid')->default(DB::raw('(UUID())'))->primary();
+            $table->string('result')->nullable();
+            $table->string('message')->nullable();
+            $table->string('bill_payment_id')->nullable();
+            $table->string('bill_refer_id')->nullable();
+            $table->string('bill_id')->nullable();
+            $table->string('bill_name')->nullable();
+            $table->string('bill_no')->nullable();
+            $table->string('biller_acc_no')->nullable();
+            $table->string('biller_mobile')->nullable();
+            $table->string('bill_from')->nullable();
+            $table->string('bill_to')->nullable();
+            $table->string('bill_gen_date')->nullable();
+            $table->string('bill_due_date')->nullable();
+            $table->string('charge')->nullable();
+            $table->string('bill_total_amount')->nullable();
+            $table->string('transaction_id')->nullable();
+            $table->string('payment_date')->nullable();
+            $table->string('payment_status')->nullable();
+            $table->string('payment_amount')->nullable();
+            $table->string('payment_trx_id')->nullable();
+            $table->string('payment_method')->nullable();
             $table->timestamps();
         });
     }
