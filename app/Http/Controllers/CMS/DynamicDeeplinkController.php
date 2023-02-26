2f333285cdd3efe83b79afc0e4f4480f0201e581
HabiburRaju <habibur.rahaman@brainstation-23.com>
2023-02-26 15:09:52 +0600

    Fix: Deeplink Issue fix

--- app/Http/Controllers/CMS/DynamicDeeplinkController.php
+++ app/Http/Controllers/CMS/DynamicDeeplinkController.php
@@ -180,8 +180,6 @@ class DynamicDeeplinkController extends Controller
             $sectionType = 'care';
         } else if($contentData->category_name == 'content'){
             $sectionType = 'all';
-        } else if ($contentData->category_name == 'commerce') {
-            $sectionType = 'all';
         }
 
         return $this->dynamicDeeplinkService->generateDeeplink($sectionType, $contentData, $request);
