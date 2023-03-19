8d99ba3863a3302e4111730086d0dccaaf1bcbf6
Plabon Dutta <plabon.dutta@banglalink.net>
2023-03-19 20:05:22 +0600

    travel added

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -137,6 +137,7 @@ class Helper
             "ELECTRICITY"             => 'Electricity',
             "GAS"                     => 'Gas',
             "WATER"                   => 'Water',
+            "TRAVEL"                   => 'Sharetrip',
         ];
     }
 
