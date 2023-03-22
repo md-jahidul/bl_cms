5dbdc83002e3468c9dad4ef88ba97ed9fddaebd0
BS956 <jahidul@brainstation-23.com>
2023-03-22 16:44:39 +0600

    Amar offer banner banner input modify

--- resources/views/admin/amar-offer-details/index.blade.php
+++ resources/views/admin/amar-offer-details/index.blade.php
@@ -47,84 +47,94 @@
     @endforeach
 
 
-    <div class="card">
-        <div class="card-content collapse show">
-            <div class="card-body card-dashboard">
-                <h4 class="menu-title"><strong>Banner Image</strong></h4><hr>
-                <div class="card-body card-dashboard">
-                    <form role="form" action="{{ route('banner-image-upload') }}" method="POST" novalidate enctype="multipart/form-data">
-                        @csrf
-                        {{method_field('POST')}}
-                        <div class="row">
+{{--    <div class="card">--}}
+{{--        <div class="card-content collapse show">--}}
+{{--            <div class="card-body card-dashboard">--}}
+{{--                <h4 class="menu-title"><strong>Banner Image</strong></h4><hr>--}}
+{{--                <div class="card-body card-dashboard">--}}
+{{--                    <form role="form" action="{{ route('banner-image-upload') }}" method="POST" novalidate enctype="multipart/form-data">--}}
+{{--                        @csrf--}}
+{{--                        {{method_field('POST')}}--}}
+{{--                        <div class="row">--}}
 
-                            <div class="form-group col-md-6 {{ $errors->has('banner_image_url') ? ' error' : '' }}">
-                                <label for="mobileImg">Desktop View Image</label>
-                                <div class="custom-file">
-                                    <input type="hidden" name="old_web_img" value="{{ isset($bannerImage->banner_image_url) ? $bannerImage->banner_image_url : null }}">
-                                    <input type="file" name="banner_image_url" class="dropify" data-height="80" id="image"
-                                           data-default-file="{{ isset($bannerImage->banner_image_url) ?  config('filesystems.file_base_url') . $bannerImage->banner_image_url : null  }}">
-                                </div>
-                                <span class="text-primary">Please given file type (.png, .jpg)</span>
+{{--                            <div class="form-group col-md-6 {{ $errors->has('banner_image_url') ? ' error' : '' }}">--}}
+{{--                                <label for="mobileImg">Desktop View Image</label>--}}
+{{--                                <div class="custom-file">--}}
+{{--                                    <input type="hidden" name="old_web_img" value="{{ isset($bannerImage->banner_image_url) ? $bannerImage->banner_image_url : null }}">--}}
+{{--                                    <input type="file" name="banner_image_url" class="dropify" data-height="80" id="image"--}}
+{{--                                           data-default-file="{{ isset($bannerImage->banner_image_url) ?  config('filesystems.file_base_url') . $bannerImage->banner_image_url : null  }}">--}}
+{{--                                </div>--}}
+{{--                                <span class="text-primary">Please given file type (.png, .jpg)</span>--}}
 
-                                <div class="help-block"></div>
-                                @if ($errors->has('banner_image_url'))
-                                    <div class="help-block">  {{ $errors->first('banner_image_url') }}</div>
-                                @endif
-                            </div>
+{{--                                <div class="help-block"></div>--}}
+{{--                                @if ($errors->has('banner_image_url'))--}}
+{{--                                    <div class="help-block">  {{ $errors->first('banner_image_url') }}</div>--}}
+{{--                                @endif--}}
+{{--                            </div>--}}
 
-                            <div class="form-group col-md-6 {{ $errors->has('banner_mobile_view') ? ' error' : '' }}">
-                                <label for="mobileImg">Mobile View Image</label>
-                                <div class="custom-file">
-                                    <input type="hidden" name="old_mob_img" value="{{ isset($bannerImage->banner_mobile_view) ? $bannerImage->banner_mobile_view : null }}">
-                                    <input type="file" name="banner_mobile_view" class="dropify" data-height="80" id="image"
-                                           data-default-file="{{ isset($bannerImage->banner_mobile_view) ?  config('filesystems.file_base_url') . $bannerImage->banner_mobile_view : null  }}">
-                                </div>
-                                <span class="text-primary">Please given file type (.png, .jpg)</span>
+{{--                            <div class="form-group col-md-6 {{ $errors->has('banner_mobile_view') ? ' error' : '' }}">--}}
+{{--                                <label for="mobileImg">Mobile View Image</label>--}}
+{{--                                <div class="custom-file">--}}
+{{--                                    <input type="hidden" name="old_mob_img" value="{{ isset($bannerImage->banner_mobile_view) ? $bannerImage->banner_mobile_view : null }}">--}}
+{{--                                    <input type="file" name="banner_mobile_view" class="dropify" data-height="80" id="image"--}}
+{{--                                           data-default-file="{{ isset($bannerImage->banner_mobile_view) ?  config('filesystems.file_base_url') . $bannerImage->banner_mobile_view : null  }}">--}}
+{{--                                </div>--}}
+{{--                                <span class="text-primary">Please given file type (.png, .jpg)</span>--}}
 
-                                <div class="help-block"></div>
-                                @if ($errors->has('banner_mobile_view'))
-                                    <div class="help-block">  {{ $errors->first('banner_mobile_view') }}</div>
-                                @endif
-                            </div>
+{{--                                <div class="help-block"></div>--}}
+{{--                                @if ($errors->has('banner_mobile_view'))--}}
+{{--                                    <div class="help-block">  {{ $errors->first('banner_mobile_view') }}</div>--}}
+{{--                                @endif--}}
+{{--                            </div>--}}
 
-                            <div class="form-group col-md-6 {{ $errors->has('alt_text') ? ' error' : '' }}">
-                                <label for="alt_text">Alt Text</label>
-                                <input type="text" name="alt_text" id="alt_text" class="form-control" placeholder="Enter offer name in English"
-                                       value="{{ isset($bannerImage->alt_text) ? $bannerImage->alt_text : null }}">
-                                <div class="help-block"></div>
-                                @if ($errors->has('alt_text'))
-                                    <div class="help-block">{{ $errors->first('alt_text') }}</div>
-                                @endif
-                            </div>
+{{--                            <div class="form-group col-md-6 {{ $errors->has('alt_text') ? ' error' : '' }}">--}}
+{{--                                <label for="alt_text">Alt Text</label>--}}
+{{--                                <input type="text" name="alt_text" id="alt_text" class="form-control" placeholder="Enter offer name in English"--}}
+{{--                                       value="{{ isset($bannerImage->alt_text) ? $bannerImage->alt_text : null }}">--}}
+{{--                                <div class="help-block"></div>--}}
+{{--                                @if ($errors->has('alt_text'))--}}
+{{--                                    <div class="help-block">{{ $errors->first('alt_text') }}</div>--}}
+{{--                                @endif--}}
+{{--                            </div>--}}
 
 
-                            <div class="form-group col-md-6 {{ $errors->has('alt_text') ? ' error' : '' }}">
-                                <label>Banner Photo Name</label>
-                                <input type="hidden" name="old_banner_name" value="{{ isset($bannerImage->banner_name) ? $bannerImage->banner_name : null }}">
-                                <input type="text" class="form-control" name="banner_name" value="{{ isset($bannerImage->banner_name) ? $bannerImage->banner_name : null }}"
-                                       placeholder="Photo Name">
-                                <small class="text-info">
-                                    <strong>i.e:</strong> app-and-service-banner (no spaces)<br>
-                                    <strong>Note: </strong> Don't need MIME type like jpg,png
-                                </small>
-                            </div>
+{{--                            <div class="form-group col-md-6 {{ $errors->has('alt_text') ? ' error' : '' }}">--}}
+{{--                                <label>Banner Photo Name</label>--}}
+{{--                                <input type="hidden" name="old_banner_name" value="{{ isset($bannerImage->banner_name) ? $bannerImage->banner_name : null }}">--}}
+{{--                                <input type="text" class="form-control" name="banner_name" value="{{ isset($bannerImage->banner_name) ? $bannerImage->banner_name : null }}"--}}
+{{--                                       placeholder="Photo Name">--}}
+{{--                                <small class="text-info">--}}
+{{--                                    <strong>i.e:</strong> app-and-service-banner (no spaces)<br>--}}
+{{--                                    <strong>Note: </strong> Don't need MIME type like jpg,png--}}
+{{--                                </small>--}}
+{{--                            </div>--}}
 
-                            <div class="form-actions col-md-12">
-                                <div class="pull-right">
-                                    <button type="submit" class="btn btn-primary"><i
-                                            class="la la-check-square-o"></i> Save
-                                    </button>
-                                </div>
-                            </div>
+{{--                            <div class="form-actions col-md-12">--}}
+{{--                                <div class="pull-right">--}}
+{{--                                    <button type="submit" class="btn btn-primary"><i--}}
+{{--                                            class="la la-check-square-o"></i> Save--}}
+{{--                                    </button>--}}
+{{--                                </div>--}}
+{{--                            </div>--}}
 
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+{{--                        </div>--}}
+{{--                    </form>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 
 </section>
+
+<!--Banner Section-->
+@php
+    $action = [
+        'section_id' => 0,
+        'section_type' => "amar_offer"
+    ];
+
+@endphp
+@include('admin.al-banner.section', $action)
 
 @stop
 
