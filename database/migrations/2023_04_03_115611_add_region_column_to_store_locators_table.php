be356fb78a2d8e6ba027f50099b24d5f075d40da
shuvo-bs-23 <mehedi.shuvo@brainstation-23.com>
2023-04-03 12:24:48 +0600

    migration for region column in store_locators table

--- database/migrations/2023_04_03_115611_add_region_column_to_store_locators_table.php
+++ database/migrations/2023_04_03_115611_add_region_column_to_store_locators_table.php
@@ -26,7 +26,7 @@ class AddRegionColumnToStoreLocatorsTable extends Migration
     public function down()
     {
         Schema::table('store_locators', function (Blueprint $table) {
-            //
+            $table->dropColumn('region');
         });
     }
 }
