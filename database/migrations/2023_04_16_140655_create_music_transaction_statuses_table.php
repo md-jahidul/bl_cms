66b5a3a3c9e02f3b075abdbe9531d0a3c2c53610
HabiburRaju <habibur.rahaman@brainstation-23.com>
2023-04-16 15:26:06 +0600

    Migration Done

---
+++ database/migrations/2023_04_16_140655_create_music_transaction_statuses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMusicTransactionStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('music_transaction_statuses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('subscription_request_id')->nullable();
+            $table->string('action_type')->nullable();
+            $table->string('action_message')->nullable();
+            $table->string('payment_id')->nullable();
+            $table->string('msisdn')->nullable();
+            $table->string('service_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('music_transaction_statuses');
+    }
+}
