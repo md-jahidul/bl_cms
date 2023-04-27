48aaf7e97979792478fe49dab0a797cb22efc356
HabiburRaju <habibur.rahaman@brainstation-23.com>
2023-04-27 13:03:23 +0600

    Progress: Phase 1 done

--- resources/views/admin/mybl-campaign/new-campaign-modality/partials/all-deno.blade.php
+++ resources/views/admin/mybl-campaign/new-campaign-modality/partials/all-deno.blade.php
@@ -47,3 +47,24 @@
     <input type="number" name="campaign_details[{{ $index }}][max_amount]" class="form-control"
            placeholder="Please Enter Max Amount" value="{{ isset($product->max_amount) ? $product->max_amount : '' }}">
 </div>
+
+<div class="form-group col-md-4">
+    <label for="numbers_of_get_bonus">No of Get Bonus Product</label>
+    <input type="number" name="campaign_details[{{ $index }}][numbers_of_get_bonus]" class="form-control"
+           placeholder="Please Select numbers of get bonus product" value="{{ isset($product->numbers_of_get_bonus) ? $product->numbers_of_get_bonus : '' }}">
+</div>
+
+@php
+    $productType = '<span class="text-success">(Prepaid) </span>'
+@endphp
+<div class="form-group col-md-4 product_code" id="cta_action">
+    <label for="bonus_product_code" class="required">Bonus Product Code</label>
+    <select id="bonus_product_code" name="campaign_details[{{ $index }}][bonus_product_code]" class=" custom-select product-list">
+        <option value="">Select Product</option>
+        @foreach ($products as $key => $value)
+            <option value="{{ $value->product_code }}"{{ isset($product->bonus_product_code) && $product->bonus_product_code == $value->product_code ? 'selected' : '' }}
+            >{!! ($value->sim_type == 1 ? $productType : "(Postpaid) ") . $value->commercial_name_en . " / " . $value->product_code  !!}</option>
+        @endforeach
+    </select>
+    <div class="help-block"></div>
+</div>
