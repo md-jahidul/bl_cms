4ea6cc16eea643796b91969ca94b9bea45ce1cdd
BS956 <jahidul@brainstation-23.com>
2023-06-04 15:44:29 +0600

    Packages details view change

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -57,7 +57,7 @@
 {{--                                            // Other Details ==============================================--}}
                                         @if(
                                             $product->offer_category->alias == "others" ||
-                                            $product->offer_category->alias == 'packages' ||
+//                                            $product->offer_category->alias == 'packages' ||
                                             $product->offer_category->alias == 'new_sim_offer' ||
                                             $product->offer_category->alias == 'bondho_sim'
                                         )
