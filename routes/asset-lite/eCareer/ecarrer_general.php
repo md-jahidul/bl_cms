f0f7af4d2b893aa6ee7fe15a9336c270195114e3
BS956 <jahidul@brainstation-23.com>
2023-07-09 12:10:54 +0600

    Add missing file for ecarrer_general

---
+++ routes/asset-lite/eCareer/ecarrer_general.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('life-at-banglalink/general',
+    'AssetLite\EcareerController@generalIndex')->name('life.at.banglalink.general');
+Route::get('life-at-banglalink/general/create',
+    'AssetLite\EcareerController@generalCreate')->name('life.at.banglalink.general.create');
+Route::post('life-at-banglalink/general/store',
+    'AssetLite\EcareerController@generalStore')->name('life.at.banglalink.general.store');
+Route::get('life-at-banglalink/general/{id}/edit',
+    'AssetLite\EcareerController@generalEdit')->name('life.at.banglalink.general.edit');
+Route::post('life-at-banglalink/general/{id}/update',
+    'AssetLite\EcareerController@generalUpdate')->name('life.at.banglalink.general.update');
+Route::get('life-at-banglalink/general/destroy/{id}',
+    'AssetLite\EcareerController@generalDestroy')->name('life.at.banglalink.general.destroy');
+Route::get('life-at-banglalink/general/sort', 'AssetLite\EcareerController@generalSort')->name('life.at.banglalink.general.sort');
+
+Route::get('life-at-banglalink/general/{id}/list', 'AssetLite\EcareerController@generalChildIndex')->name('life.at.banglalink.general.child.index');
+?>
