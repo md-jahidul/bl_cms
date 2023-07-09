72befb1e79d733c50890158566f18731e639193b
BS956 <jahidul@brainstation-23.com>
2023-07-09 12:21:16 +0600

    Conflict resolved in OtherDynamicPage

--- app/Models/OtherDynamicPage.php
+++ app/Models/OtherDynamicPage.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class OtherDynamicPage extends Model
 {
@@ -11,14 +12,14 @@ class OtherDynamicPage extends Model
 
     protected $fillable = [
         'page_header', 'page_header_bn', 'schema_markup',
-        'banner_image_url', 'banner_mobile_view',
+        'banner_image_url', 'banner_mobile_view', 'type',
         'alt_text', 'alt_text_bn', 'banner_name', 'banner_name_bn',
         'page_name_en', 'page_name_bn', 'url_slug', 'url_slug_bn',
         'page_content_en', 'page_content_bn', 'created_by', 'updated_by'
     ];
 
-    public function searchableFeature(): MorphMany
+    public function searchableFeature(): MorphOne
     {
-        return $this->morphMany(SearchableData::class, 'featureable');
+        return $this->morphOne(SearchableData::class, 'featureable');
     }
 }
