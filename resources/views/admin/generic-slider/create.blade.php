0bdc5110ecf9da1e192dce1d72da198b65ec81f4
HabiburRaju <habibur.rahaman@brainstation-23.com>
2023-08-02 12:44:37 +0600

    Toffee section slider key added.

--- resources/views/admin/generic-slider/create.blade.php
+++ resources/views/admin/generic-slider/create.blade.php
@@ -63,6 +63,12 @@
                                             {{ (isset($single_slider->component_for) && $single_slider->component_for == 'lms') ? 'checked' : '' }}
                                             {{ isset($single_slider->component_for) ? '' : 'checked' }}>
                                         <label for="campaignStatusInactive" class="mr-3">LMS</label>
+                                        <input type="radio" name="component_for" value="toffee" id="campaignStatusInactive"
+                                            {{ (isset($single_slider->component_for) && $single_slider->component_for == 'toffee') ? 'checked' : '' }}>
+                                        <label for="campaignStatusInactive" class="mr-3">Toffee Home</label>
+                                        <input type="radio" name="component_for" value="toffee_section" id="campaignStatusInactive"
+                                            {{ (isset($single_slider->component_for) && $single_slider->component_for == 'toffee_section') ? 'checked' : '' }}>
+                                        <label for="campaignStatusInactive" class="mr-3">Toffee Section</label>
                                         @if ($errors->has('component_for'))
                                             <div class="help-block">  {{ $errors->first('component_for') }}</div>
                                         @endif
