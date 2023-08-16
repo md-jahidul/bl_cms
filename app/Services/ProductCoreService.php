a7fc49776e6dd9e2312beb973a85387a02c94a92
HabiburRaju <habibur.rahaman@brainstation-23.com>
2023-08-17 01:59:13 +0600

    Tag Id issue fix.

--- app/Services/ProductCoreService.php
+++ app/Services/ProductCoreService.php
@@ -396,7 +396,6 @@ class ProductCoreService
                                 $existingTagTitles = array_map('strtolower', $existingTagTitles);
                                 $existingTagIds = $existingTags->pluck('id')->toArray();
 
-
                                 foreach ($tags as $tag) {
                                     if (!in_array(strtolower($tag), Arr::flatten($existingTagTitles)) && $tag != "") {
                                         $tagInsert = new ProductTag();
@@ -420,7 +419,8 @@ class ProductCoreService
 
                                 $this->syncProductTags($product_code, Arr::flatten($existingTagIds));
                             }
-                            if (count($tags) == 0) {
+
+                            if (count($tags) == 0 || (count($tags) == 1 && $tags[0] == "")) {
                                 MyBlProduct::where('product_code', $product_code)->update(['tag_id' => null]);
                             }
 
