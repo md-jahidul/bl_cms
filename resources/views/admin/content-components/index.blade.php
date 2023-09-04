b9eaa3eafbf2c64c494b44f7bc09f2256d9a00bf
HabiburRaju <habibur.rahaman@brainstation-23.com>
2023-09-04 09:42:10 +0600

    Home Component DOne.

--- resources/views/admin/content-components/index.blade.php
+++ resources/views/admin/content-components/index.blade.php
@@ -108,8 +108,16 @@
                                             <div class="help-block">  {{ $errors->first('title_bn') }}</div>
                                         @endif
                                     </div>
-
-                                    <div class="col-md-6">
+                                    <div class="form-group col-md-4 {{ $errors->has('version_code') ? ' error' : '' }}">
+                                        <label for="title" class="required">Version Code</label>
+                                        <input type="number" name="version_code"  class="form-control" placeholder="Enter Version Code"
+                                               required data-validation-required-message="Enter Version Code">
+                                        <div class="help-block"></div>
+                                        @if ($errors->has('version_code'))
+                                            <div class="help-block">  {{ $errors->first('version_code') }}</div>
+                                        @endif
+                                    </div>
+                                    <div class="col-md-4">
                                         <div class="form-group">
                                             <label for="title" class="required mr-1">User Can Enable/Disable:</label> <br>
                                             <input type="radio" name="is_eligible" value="1" id="active">
@@ -120,9 +128,9 @@
                                         </div>
                                     </div>
 
-                                    <div class="col-md-6">
+                                    <div class="col-md-4">
                                         <div class="form-group">
-                                            <label for="title" class="required mr-1">Status:</label>
+                                            <label for="title" class="required mr-1">Status:</label> <br>
 
                                             <input type="radio" name="is_api_call_enable" value="1" id="can_enable_yes">
                                             <label for="can_enable_yes" class="mr-1">Enable</label>
@@ -164,6 +172,7 @@
         (function () {
             let titleEn = $("input[name='title_en']");
             let titleBn = $("input[name='title_bn']");
+            let versionCode = $("input[name='version_code']");
             let active = $("#active");
             let inactive = $("#inactive");
             let enable_yes = $("#can_enable_yes");
@@ -192,6 +201,7 @@
                     success: function (data) {
                         titleEn.val(data.title_en)
                         titleBn.val(data.title_bn)
+                        versionCode.val(data.version_code)
                         if (data.is_eligible === 1){
                             active.prop("checked", true)
                         } else {
