4811fd8221470c49d837cef6679c88434f2d8d01
BS956 <jahidul@brainstation-23.com>
2023-09-17 14:44:14 +0600

    Issue fix

--- resources/views/layouts/partials/left_menu/mybl.blade.php
+++ resources/views/layouts/partials/left_menu/mybl.blade.php
@@ -1104,11 +1104,12 @@
 
 @endif
 
+{{--@if (Auth::user()->hasRole('developer'))--}}
     <li class="{{ is_active_url('developer/api/debug') }}">
         <a class="menu-item" href="{{ route('support-message') }}">
             <i class="la la-code-fork"></i>Support Messages</a>
     </li>
-@endif
+{{--@endif--}}
 
 
 {{--------------------------------------------------------------------------------------------------------------------}}
