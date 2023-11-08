e2babc718480e2dc68f53c506d896a2572d22a42
shuvo-bs-23 <mehedi.shuvo@brainstation-23.com>
2023-11-08 18:08:18 +0600

    Onmobile transaction status route in menu

--- resources/views/layouts/partials/left_menu/mybl.blade.php
+++ resources/views/layouts/partials/left_menu/mybl.blade.php
@@ -989,6 +989,12 @@
                     <i class="la la-list"></i>DocTime Transaction
                 </a>
             </li>
+            <li class="{{is_active_url('mybl/onmobile/transaction-status-report-view')}}">
+                <a class="menu-item" href="{{ route('mybl.transaction-status',['type' => 'onmobile']) }} "
+                   data-i18n="nav.templates.vert.classic_menu">
+                    <i class="la la-list"></i>Onmobile Transaction
+                </a>
+            </li>
         </ul>
     </li>
 
