cbf0efaf8c7c1e53e907f9d800087f8eab9d753e
shuvo-bs-23 <mehedi.shuvo@brainstation-23.com>
2023-11-12 14:29:37 +0600

    user_ip, version, platform, device_token added in migration file

--- database/migrations/2023_11_06_140914_create_mybl_sim_bar_unbar_table.php
+++ database/migrations/2023_11_06_140914_create_mybl_sim_bar_unbar_table.php
@@ -18,10 +18,14 @@ class CreateMyblSimBarUnbarTable extends Migration
             $table->string('transaction_id')->unique();
             $table->string('requester_msisdn');
             $table->string('action_msisdn');
-            $table->string('current_status');
-            $table->string('action_status');
-            $table->string('status');
-            $table->dateTime('expires_at');
+            $table->string('current_status')->nullable();
+            $table->string('action_status')->nullable();
+            $table->string('status')->nullable();
+            $table->dateTime('expires_at')->nullable();
+            $table->string('user_ip')->nullable();
+            $table->string('version')->nullable();
+            $table->string('platform')->nullable();
+            $table->string('device_token')->nullable();
             $table->timestamps();
         });
     }
