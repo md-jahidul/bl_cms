4f825ee482699389d73b6383f9d39eec2942c0c4
shuvo-bs-23 <mehedi.shuvo@brainstation-23.com>
2023-12-14 11:36:47 +0600

    migration file for vas products

---
+++ database/migrations/2023_12_14_103300_create_mybl_vas_products_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMyblVasProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mybl_vas_products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('partner_id');
+            $table->string('cp_id');
+            $table->string('title_en');
+            $table->string('title_bn');
+            $table->string('desc_en');
+            $table->string('desc_bn');
+            $table->double('price', 8, 2);
+            $table->string('validity_en');
+            $table->string('validity_bn');
+            $table->string('image')->nullable();
+            $table->string('platform')->nullable();
+            $table->boolean('is_renewal')->default(0);
+            $table->boolean('status')->default(0);
+            $table->string('activation_type')->default('Default');
+            $table->string('deactivation_type')->default('Default');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mybl_vas_products');
+    }
+}
