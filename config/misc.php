22e322efb706af22cf680832855857ce85954da6
BS956 <jahidul@brainstation-23.com>
2024-02-28 19:26:01 +0600

    Add config key for DXP enable or disable

--- config/misc.php
+++ config/misc.php
@@ -4,5 +4,6 @@ return [
     'migrator' => [
         'product_list' => env('PRODUCT_LIST_VERSION', 'V2'),
         'config' => env('CONFIG_VERSION', 'V2'),
+        'dxp_new_login' => env('DXP_NEW_LOGIN', true),
     ],
 ];
