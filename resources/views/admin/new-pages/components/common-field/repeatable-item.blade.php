02fde9b6639f8c6d64f716b4e3741e7e34038cfd
BS956 <jahidul@brainstation-23.com>
2024-03-18 14:48:14 +0600

    Add button for tab component five

--- resources/views/admin/new-pages/components/common-field/repeatable-item.blade.php
+++ resources/views/admin/new-pages/components/common-field/repeatable-item.blade.php
@@ -119,6 +119,7 @@
                                 @include('admin.new-pages.components.common-field.multi-item.image', ['is_tab' => true, 'tabIndex' => $tabIndex])
                                 @include('admin.new-pages.components.common-field.multi-item.title', ['is_tab' => true, 'tabIndex' => $tabIndex])
                                 @include('admin.new-pages.components.common-field.multi-item.description', ['is_tab' => true, 'tabIndex' => $tabIndex, 'is_editor' => true])
+                                @include('admin.new-pages.components.common-field.multi-item.button', ['is_tab' => true, 'tabIndex' => $tabIndex])
                             @else
                                 @include('admin.new-pages.components.common-field.multi-item.title', ['is_tab' => true, 'tabIndex' => $tabIndex])
                                 @include('admin.new-pages.components.common-field.multi-item.description', ['is_tab' => true, 'tabIndex' => $tabIndex, 'is_editor' => true])
